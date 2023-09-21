d4ce897e50cf91d2d9192ea5c7ca43708a2c654a
zetienne <zachetie@gmail.com>
2023-09-20 19:43:33 -0700

    general_relativity: Add psi4 support

---
+++ nrpy/equations/general_relativity/psi4.py
@@ -0,0 +1,322 @@
+"""
+This module constructs psi_4, with
+respect to any tetrad
+
+Author: Zachariah B. Etienne
+        zachetie **at** gmail **dot* com
+"""
+
+# Step 1.a: import all needed modules from NRPy+:
+from typing import List, cast
+import sympy as sp  # SymPy: The Python computer algebra package upon which NRPy+ depends
+import nrpy.indexedexp as ixp  # NRPy+: Symbolic indexed expression (e.g., tensors, vectors, etc.) support
+from nrpy.equations.general_relativity.BSSN_to_ADM import BSSN_to_ADM
+from nrpy.equations.general_relativity.psi4_tetrads import Psi4Tetrads
+
+
+class Psi4:
+    """
+    Class responsible for constructing psi_4 with respect to any specified tetrad.
+
+    :param CoordSystem: The coordinate system to be used. Default is 'Cartesian'.
+    :param enable_rfm_precompute: Flag to enable/disable reference metric precomputation. Default is False.
+    :param specify_tetrad: Specify the tetrad explicitly? Default is True. False means to leave it symbolic.
+    """
+
+    def __init__(
+        self,
+        CoordSystem: str = "Cartesian",
+        enable_rfm_precompute: bool = False,
+        specify_tetrad: bool = True,
+    ):
+        # Step 1.d: Import all ADM quantities as written in terms of BSSN quantities
+        BtoA = BSSN_to_ADM(
+            CoordSystem=CoordSystem,
+            enable_rfm_precompute=enable_rfm_precompute,
+        )
+
+        # Step 1.e: Set up tetrad vectors
+        mre4U: List[sp.Expr]
+        mim4U: List[sp.Expr]
+        n4U: List[sp.Expr]
+        if specify_tetrad:
+            BP4t = Psi4Tetrads(
+                CoordSystem=CoordSystem, enable_rfm_precompute=enable_rfm_precompute
+            )
+            mre4U = BP4t.mre4U
+            mim4U = BP4t.mim4U
+            n4U = BP4t.n4U
+        else:
+            # For code validation against NRPy+ psi_4 tutorial module (Tutorial-Psi4.ipynb);
+            #   ensures a more complete code validation.
+            mre4U = cast(List[sp.Expr], ixp.declarerank1("mre4U", dimension=4))
+            mim4U = cast(List[sp.Expr], ixp.declarerank1("mim4U", dimension=4))
+            n4U = cast(List[sp.Expr], ixp.declarerank1("n4U", dimension=4))
+
+        # Step 2: Construct the (rank-4) Riemann curvature tensor associated with the ADM 3-metric:
+        RDDDD = ixp.zerorank4()
+        gammaDDdDD = BtoA.gammaDDdDD
+
+        for i in range(3):
+            for k in range(3):
+                for l in range(3):
+                    for m in range(3):
+                        RDDDD[i][k][l][m] = sp.Rational(1, 2) * (
+                            +gammaDDdDD[i][m][k][l]
+                            + gammaDDdDD[k][l][i][m]
+                            - gammaDDdDD[i][l][k][m]
+                            - gammaDDdDD[k][m][i][l]
+                        )
+
+        # ... then we add the term on the right:
+        gammaDD = BtoA.gammaDD
+        GammaUDD = BtoA.GammaUDD
+
+        for i in range(3):
+            for k in range(3):
+                for l in range(3):
+                    for m in range(3):
+                        for n in range(3):
+                            for p in range(3):
+                                RDDDD[i][k][l][m] += gammaDD[n][p] * (
+                                    GammaUDD[n][k][l] * GammaUDD[p][i][m]
+                                    - GammaUDD[n][k][m] * GammaUDD[p][i][l]
+                                )
+
+        # Step 3: Construct the (rank-4) tensor in term 1 of psi_4 (referring to Eq 5.1 in
+        #   Baker, Campanelli, Lousto (2001); https://arxiv.org/pdf/gr-qc/0104063.pdf
+        rank4term1DDDD = ixp.zerorank4()
+        KDD = BtoA.KDD
+
+        for i in range(3):
+            for j in range(3):
+                for k in range(3):
+                    for l in range(3):
+                        rank4term1DDDD[i][j][k][l] = (
+                            RDDDD[i][j][k][l]
+                            + KDD[i][k] * KDD[l][j]
+                            - KDD[i][l] * KDD[k][j]
+                        )
+
+        # Step 4: Construct the (rank-3) tensor in term 2 of psi_4 (referring to Eq 5.1 in
+        #   Baker, Campanelli, Lousto (2001); https://arxiv.org/pdf/gr-qc/0104063.pdf
+        rank3term2DDD = ixp.zerorank3()
+        KDDdD = BtoA.KDDdD
+
+        for j in range(3):
+            for k in range(3):
+                for l in range(3):
+                    rank3term2DDD[j][k][l] = sp.Rational(1, 2) * (
+                        KDDdD[j][k][l] - KDDdD[j][l][k]
+                    )
+
+        # ... then we construct the second term in this sum:
+        #  \Gamma^{p}_{j[k} K_{l]p} = \frac{1}{2} (\Gamma^{p}_{jk} K_{lp}-\Gamma^{p}_{jl} K_{kp}):
+        for j in range(3):
+            for k in range(3):
+                for l in range(3):
+                    for p in range(3):
+                        rank3term2DDD[j][k][l] += sp.Rational(1, 2) * (
+                            GammaUDD[p][j][k] * KDD[l][p]
+                            - GammaUDD[p][j][l] * KDD[k][p]
+                        )
+
+        # Finally, we multiply the term by $-8$:
+        for j in range(3):
+            for k in range(3):
+                for l in range(3):
+                    rank3term2DDD[j][k][l] *= sp.sympify(-8)
+
+        # Step 5: Construct the (rank-2) tensor in term 3 of psi_4 (referring to Eq 5.1 in
+        #   Baker, Campanelli, Lousto (2001); https://arxiv.org/pdf/gr-qc/0104063.pdf
+
+        # Step 5.1: Construct 3-Ricci tensor R_{ij} = gamma^{im} R_{ijml}
+        RDD = ixp.zerorank2()
+        gammaUU = BtoA.gammaUU
+        for j in range(3):
+            for l in range(3):
+                for i in range(3):
+                    for m in range(3):
+                        RDD[j][l] += gammaUU[i][m] * RDDDD[i][j][m][l]
+
+        # Step 5.2: Construct K^p_l = gamma^{pi} K_{il}
+        KUD = ixp.zerorank2()
+        for p in range(3):
+            for l in range(3):
+                for i in range(3):
+                    KUD[p][l] += gammaUU[p][i] * KDD[i][l]
+
+        # Step 5.3: Construct trK = gamma^{ij} K_{ij}
+        trK = sp.sympify(0)
+        for i in range(3):
+            for j in range(3):
+                trK += gammaUU[i][j] * KDD[i][j]
+
+        # Next we put these terms together to construct the entire term in parentheses:
+        # +4 \left(R_{jl} - K_{jp} K^p_l + K K_{jl} \right),
+        rank2term3DD = ixp.zerorank2()
+        for j in range(3):
+            for l in range(3):
+                rank2term3DD[j][l] = RDD[j][l] + trK * KDD[j][l]
+                for p in range(3):
+                    rank2term3DD[j][l] += -KDD[j][p] * KUD[p][l]
+        # Finally we multiply by +4:
+        for j in range(3):
+            for l in range(3):
+                rank2term3DD[j][l] *= sp.sympify(4)
+
+        # Step 6: Construct real & imaginary parts of psi_4
+        #         by contracting constituent rank 2, 3, and 4
+        #         tensors with input tetrads mre4U, mim4U, & n4U.
+
+        def tetrad_product__Real_psi4(
+            n: List[sp.Expr],
+            Mre: List[sp.Expr],
+            Mim: List[sp.Expr],
+            mu: int,
+            nu: int,
+            eta: int,
+            delta: int,
+        ) -> sp.Expr:
+            return cast(
+                sp.Expr,
+                +n[mu] * Mre[nu] * n[eta] * Mre[delta]
+                - n[mu] * Mim[nu] * n[eta] * Mim[delta],
+            )
+
+        def tetrad_product__Imag_psi4(
+            n: List[sp.Expr],
+            Mre: List[sp.Expr],
+            Mim: List[sp.Expr],
+            mu: int,
+            nu: int,
+            eta: int,
+            delta: int,
+        ) -> sp.Expr:
+            return cast(
+                sp.Expr,
+                -n[mu] * Mre[nu] * n[eta] * Mim[delta]
+                - n[mu] * Mim[nu] * n[eta] * Mre[delta],
+            )
+
+        # We split psi_4 into three pieces, to expedite & possibly parallelize C code generation.
+        self.psi4_re_pt = [sp.sympify(0), sp.sympify(0), sp.sympify(0)]
+        self.psi4_im_pt = [sp.sympify(0), sp.sympify(0), sp.sympify(0)]
+        # First term:
+        for i in range(3):
+            for j in range(3):
+                for k in range(3):
+                    for l in range(3):
+                        self.psi4_re_pt[0] += rank4term1DDDD[i][j][k][
+                            l
+                        ] * tetrad_product__Real_psi4(
+                            n4U, mre4U, mim4U, i + 1, j + 1, k + 1, l + 1
+                        )
+                        self.psi4_im_pt[0] += rank4term1DDDD[i][j][k][
+                            l
+                        ] * tetrad_product__Imag_psi4(
+                            n4U, mre4U, mim4U, i + 1, j + 1, k + 1, l + 1
+                        )
+
+        # Second term:
+        for j in range(3):
+            for k in range(3):
+                for l in range(3):
+                    self.psi4_re_pt[1] += (
+                        rank3term2DDD[j][k][l]
+                        * sp.Rational(1, 2)
+                        * (
+                            +tetrad_product__Real_psi4(
+                                n4U, mre4U, mim4U, 0, j + 1, k + 1, l + 1
+                            )
+                            - tetrad_product__Real_psi4(
+                                n4U, mre4U, mim4U, j + 1, 0, k + 1, l + 1
+                            )
+                        )
+                    )
+                    self.psi4_im_pt[1] += (
+                        rank3term2DDD[j][k][l]
+                        * sp.Rational(1, 2)
+                        * (
+                            +tetrad_product__Imag_psi4(
+                                n4U, mre4U, mim4U, 0, j + 1, k + 1, l + 1
+                            )
+                            - tetrad_product__Imag_psi4(
+                                n4U, mre4U, mim4U, j + 1, 0, k + 1, l + 1
+                            )
+                        )
+                    )
+        # Third term:
+        for j in range(3):
+            for l in range(3):
+                self.psi4_re_pt[2] += rank2term3DD[j][l] * (
+                    sp.Rational(1, 4)
+                    * (
+                        +tetrad_product__Real_psi4(
+                            n4U, mre4U, mim4U, 0, j + 1, 0, l + 1
+                        )
+                        - tetrad_product__Real_psi4(
+                            n4U, mre4U, mim4U, j + 1, 0, 0, l + 1
+                        )
+                        - tetrad_product__Real_psi4(
+                            n4U, mre4U, mim4U, 0, j + 1, l + 1, 0
+                        )
+                        + tetrad_product__Real_psi4(
+                            n4U, mre4U, mim4U, j + 1, 0, l + 1, 0
+                        )
+                    )
+                )
+                self.psi4_im_pt[2] += rank2term3DD[j][l] * (
+                    sp.Rational(1, 4)
+                    * (
+                        +tetrad_product__Imag_psi4(
+                            n4U, mre4U, mim4U, 0, j + 1, 0, l + 1
+                        )
+                        - tetrad_product__Imag_psi4(
+                            n4U, mre4U, mim4U, j + 1, 0, 0, l + 1
+                        )
+                        - tetrad_product__Imag_psi4(
+                            n4U, mre4U, mim4U, 0, j + 1, l + 1, 0
+                        )
+                        + tetrad_product__Imag_psi4(
+                            n4U, mre4U, mim4U, j + 1, 0, l + 1, 0
+                        )
+                    )
+                )
+
+
+if __name__ == "__main__":
+    import doctest
+    import os
+    import sys
+    import nrpy.validate_expressions.validate_expressions as ve
+
+    results = doctest.testmod()
+    if results.failed > 0:
+        print(f"Doctest failed: {results.failed} of {results.attempted} test(s)")
+        sys.exit(1)
+    else:
+        print(f"Doctest passed: All {results.attempted} test(s) passed")
+
+    for Coord in [
+        "Spherical",
+        "SinhSpherical",
+        "SinhSpherical_rfm_precompute",
+    ]:
+        enable_rfm_precompute = False
+        Coord_in = Coord
+        if "rfm_precompute" in Coord:
+            Coord_in = Coord.replace("_rfm_precompute", "")
+            enable_rfm_precompute = True
+        psi4 = Psi4(Coord_in, enable_rfm_precompute)
+        results_dict = ve.process_dictionary_of_expressions(
+            psi4.__dict__, fixed_mpfs_for_free_symbols=True
+        )
+        ve.compare_or_generate_trusted_results(
+            os.path.abspath(__file__),
+            os.getcwd(),
+            # File basename. If this is set to "trusted_module_test1", then
+            #   trusted results_dict will be stored in tests/trusted_module_test1.py
+            f"{os.path.splitext(os.path.basename(__file__))[0]}_{Coord}",
+            results_dict,
+        )
---
+++ nrpy/equations/general_relativity/psi4_tetrads.py
@@ -0,0 +1,280 @@
+"""
+This module constructs tetrads needed to
+compute psi_4 (as well as other Weyl scalars
+and invariants if desired).
+
+Authors: Zachariah B. Etienne
+         (zachetie **at** gmail **dot* com),
+         and Patrick Nelson
+"""
+
+# Step 1.a: import all needed modules from NRPy+:
+from typing import List, cast, Union
+import sympy as sp  # SymPy: The Python computer algebra package upon which NRPy+ depends
+import nrpy.params as par  # NRPy+: Parameter interface
+import nrpy.indexedexp as ixp  # NRPy+: Symbolic indexed expression (e.g., tensors, vectors, etc.) support
+import nrpy.reference_metric as refmetric  # NRPy+: Reference metric support
+from nrpy.equations.general_relativity.BSSN_quantities import BSSN_quantities
+from nrpy.equations.general_relativity.BSSN_to_ADM import BSSN_to_ADM
+
+# Step 1.b: Initialize TetradChoice parameter
+# Current option: quasiKinnersley = choice made in Baker, Campanelli, and Lousto. PRD 65, 044001 (2002)
+par.register_param(str, __name__, "tetrad_choice", "quasiKinnersley")
+# use_metric_to_construct_unit_normal=False: consistent with WeylScal4 ETK thorn.
+par.register_param(bool, __name__, "use_metric_to_construct_unit_normal", False)
+
+
+class Psi4Tetrads:
+    """
+    Class responsible for constructing tetrads needed for the computation
+    of Weyl scalars like psi_4, and other GR invariants.
+
+    :param CoordSystem: The coordinate system to be used. Default is 'Cartesian'.
+    :param enable_rfm_precompute: Flag to enable/disable reference metric precomputation. Default is False.
+
+    :raises ValueError: If an unsupported tetrad choice is made.
+    """
+
+    def __init__(
+        self, CoordSystem: str = "Cartesian", enable_rfm_precompute: bool = False
+    ):
+        # Step 1.c: Check if tetrad choice is implemented:
+        self.tetrad_choice = par.parval_from_str("tetrad_choice")
+        if self.tetrad_choice != "quasiKinnersley":
+            raise ValueError(
+                f"ERROR: {__name__}::TetradChoice = {self.tetrad_choice} currently unsupported!"
+            )
+
+        # Step 1.d: Given the chosen coordinate system, set up
+        #           corresponding reference metric and needed
+        #           reference metric quantities
+        # The following function call sets up the reference metric
+        #    and related quantities, including rescaling matrices ReDD,
+        #    ReU, and hatted quantities.
+        rfm = refmetric.reference_metric[
+            CoordSystem + "_rfm_precompute" if enable_rfm_precompute else CoordSystem
+        ]
+
+        # Step 1.f: Import all ADM quantities as written in terms of BSSN quantities
+        BtoA = BSSN_to_ADM(
+            CoordSystem=CoordSystem, enable_rfm_precompute=enable_rfm_precompute
+        )
+
+        # Step 2.a: Declare the Cartesian x,y,z in terms of
+        #           xx0,xx1,xx2.
+        x = rfm.xx_to_Cart[0]
+        y = rfm.xx_to_Cart[1]
+        z = rfm.xx_to_Cart[2]
+
+        # Step 2.b: Declare detgamma and gammaUU from
+        #           BSSN.ADM_in_terms_of_BSSN;
+        #           simplify detgamma & gammaUU expressions,
+        #           which expedites Psi4 codegen.
+        detgamma = sp.simplify(BtoA.detgamma)
+        gammaUU = ixp.zerorank2()
+        for i in range(3):
+            for j in range(3):
+                gammaUU[i][j] = sp.simplify(BtoA.gammaUU[i][j])
+
+        # Step 2.c: Define v1U and v2U
+        v1UCart = [-y, x, sp.sympify(0)]
+        v2UCart = [x, y, z]
+
+        # Step 2.d: Construct the Jacobian d x_Cart^i / d xx^j
+        Jac_dUCart_dDrfmUD = ixp.zerorank2()
+        for i in range(3):
+            for j in range(3):
+                Jac_dUCart_dDrfmUD[i][j] = sp.simplify(
+                    sp.diff(rfm.xx_to_Cart[i], rfm.xx[j])
+                )
+
+        # Step 2.e: Invert above Jacobian to get needed d xx^j / d x_Cart^i
+        Jac_dUrfm_dDCartUD, dummyDET = ixp.generic_matrix_inverter3x3(
+            Jac_dUCart_dDrfmUD
+        )
+
+        # Step 2.e.i: Simplify expressions for d xx^j / d x_Cart^i:
+        for i in range(3):
+            for j in range(3):
+                Jac_dUrfm_dDCartUD[i][j] = sp.simplify(Jac_dUrfm_dDCartUD[i][j])
+
+        # Step 2.f: Transform v1U and v2U from the Cartesian to the xx^i basis
+        v1U = ixp.zerorank1()
+        v2U = ixp.zerorank1()
+        for i in range(3):
+            for j in range(3):
+                v1U[i] += Jac_dUrfm_dDCartUD[i][j] * v1UCart[j]
+                v2U[i] += Jac_dUrfm_dDCartUD[i][j] * v2UCart[j]
+
+        # Step 2.g: Define v3U
+        v3U = ixp.zerorank1()
+        LeviCivitaSymbolDDD = ixp.LeviCivitaSymbol_dim3_rank3()
+        for a in range(3):
+            for b in range(3):
+                for c in range(3):
+                    for d in range(3):
+                        v3U[a] += (
+                            sp.sqrt(detgamma)
+                            * gammaUU[a][d]
+                            * LeviCivitaSymbolDDD[d][b][c]
+                            * v1U[b]
+                            * v2U[c]
+                        )
+
+        # Step 2.g.i: Simplify expressions for v1U,v2U,v3U. This greatly expedites the C code generation (~10x faster)
+        #             Drat. Simplification with certain versions of SymPy & coord systems results in a hang. Let's just
+        #             evaluate the expressions so the most trivial optimizations can be performed.
+        for a in range(3):
+            v1U[a] = v1U[a].doit()  # sp.simplify(v1U[a])
+            v2U[a] = v2U[a].doit()  # sp.simplify(v2U[a])
+            v3U[a] = v3U[a].doit()  # sp.simplify(v3U[a])
+
+        # Step 2.h: Define omega_{ij}
+        omegaDD = ixp.zerorank2()
+        gammaDD = BtoA.gammaDD
+
+        def v_vectorDU(
+            v1U: List[sp.Expr], v2U: List[sp.Expr], v3U: List[sp.Expr], i: int, a: int
+        ) -> sp.Expr:
+            if i == 0:
+                return v1U[a]
+            if i == 1:
+                return v2U[a]
+            if i == 2:
+                return v3U[a]
+            raise ValueError("ERROR: unknown vector!")
+
+        def update_omega(
+            omegaDD: List[List[sp.Expr]],
+            i: int,
+            j: int,
+            v1U: List[sp.Expr],
+            v2U: List[sp.Expr],
+            v3U: List[sp.Expr],
+            gammaDD: List[List[sp.Expr]],
+        ) -> None:
+            omegaDD[i][j] = sp.sympify(0)
+            for a in range(3):
+                for b in range(3):
+                    omegaDD[i][j] += (
+                        v_vectorDU(v1U, v2U, v3U, i, a)
+                        * v_vectorDU(v1U, v2U, v3U, j, b)
+                        * gammaDD[a][b]
+                    )
+
+        # Step 2.i: Define e^a_i. Note that:
+        #           omegaDD[0][0] = \omega_{11} above;
+        #           omegaDD[1][1] = \omega_{22} above, etc.
+        # First e_1^a: Orthogonalize & normalize:
+        e1U = ixp.zerorank1()
+        update_omega(omegaDD, 0, 0, v1U, v2U, v3U, gammaDD)
+        for a in range(3):
+            e1U[a] = v1U[a] / sp.sqrt(omegaDD[0][0])
+
+        # Next e_2^a: First orthogonalize:
+        e2U = ixp.zerorank1()
+        update_omega(omegaDD, 0, 1, e1U, v2U, v3U, gammaDD)
+        for a in range(3):
+            e2U[a] = v2U[a] - omegaDD[0][1] * e1U[a]
+        # Then normalize:
+        update_omega(omegaDD, 1, 1, e1U, e2U, v3U, gammaDD)
+        for a in range(3):
+            e2U[a] /= sp.sqrt(omegaDD[1][1])
+
+        # Next e_3^a: First orthogonalize:
+        e3U = ixp.zerorank1()
+        update_omega(omegaDD, 0, 2, e1U, e2U, v3U, gammaDD)
+        update_omega(omegaDD, 1, 2, e1U, e2U, v3U, gammaDD)
+        for a in range(3):
+            e3U[a] = v3U[a] - omegaDD[0][2] * e1U[a] - omegaDD[1][2] * e2U[a]
+        # Then normalize:
+        update_omega(omegaDD, 2, 2, e1U, e2U, e3U, gammaDD)
+        for a in range(3):
+            e3U[a] /= sp.sqrt(omegaDD[2][2])
+
+        # Step 2.j: Construct l^mu, n^mu, and m^mu, based on r^mu, theta^mu, phi^mu, and u^mu:
+        r4U = cast(List[sp.Expr], ixp.zerorank1(dimension=4))
+        # u4U = cast(List[Union[int, sp.Expr]], ixp.zerorank1(dimension=4))
+        u4U = cast(List[Union[int, sp.Expr]], ixp.zerorank1(dimension=4))
+        theta4U = cast(List[sp.Expr], ixp.zerorank1(dimension=4))
+        phi4U = cast(List[sp.Expr], ixp.zerorank1(dimension=4))
+
+        for a in range(3):
+            r4U[a + 1] = e2U[a]
+            theta4U[a + 1] = e3U[a]
+            phi4U[a + 1] = e1U[a]
+
+        # FIXME? assumes alpha=1, beta^i = 0
+        if par.parval_from_str("use_metric_to_construct_unit_normal"):
+            # Eq. 2.116 in Baumgarte & Shapiro:
+            #  n^mu = {1/alpha, -beta^i/alpha}. Note that n_mu = {alpha,0}, so n^mu n_mu = -1.
+            Bq = BSSN_quantities[
+                CoordSystem + "_rfm_precompute"
+                if enable_rfm_precompute
+                else CoordSystem
+            ]
+            u4U[0] = 1 / Bq.alpha
+            for i in range(3):
+                u4U[i + 1] = -Bq.betaU[i] / Bq.alpha
+        else:
+            u4U[0] = 1
+
+        self.l4U = ixp.zerorank1(dimension=4)
+        self.n4U = ixp.zerorank1(dimension=4)
+        self.mre4U = ixp.zerorank1(dimension=4)
+        self.mim4U = ixp.zerorank1(dimension=4)
+
+        # M_SQRT1_2 = 1 / sqrt(2) (defined in math.h on Linux)
+        M_SQRT1_2 = par.register_CodeParameter(
+            "#define",
+            __name__,
+            "M_SQRT1_2",
+            "",
+            commondata=False,
+            add_to_parfile=False,
+            add_to_set_CodeParameters_h=False,
+            add_to_glb_code_params_dict=False,
+        )
+        isqrt2 = M_SQRT1_2  # 1/sp.sqrt(2) <- SymPy drops precision to 15 sig. digits in unit tests
+        for mu in range(4):
+            self.l4U[mu] = isqrt2 * (u4U[mu] + r4U[mu])
+            self.n4U[mu] = isqrt2 * (u4U[mu] - r4U[mu])
+            self.mre4U[mu] = isqrt2 * theta4U[mu]
+            self.mim4U[mu] = isqrt2 * phi4U[mu]
+
+
+if __name__ == "__main__":
+    import doctest
+    import os
+    import sys
+    import nrpy.validate_expressions.validate_expressions as ve
+
+    results = doctest.testmod()
+    if results.failed > 0:
+        print(f"Doctest failed: {results.failed} of {results.attempted} test(s)")
+        sys.exit(1)
+    else:
+        print(f"Doctest passed: All {results.attempted} test(s) passed")
+
+    for Coord in [
+        "Spherical",
+        "SinhSpherical",
+        "SinhSpherical_rfm_precompute",
+    ]:
+        enable_rfm_precompute = False
+        Coord_in = Coord
+        if "rfm_precompute" in Coord:
+            Coord_in = Coord.replace("_rfm_precompute", "")
+            enable_rfm_precompute = True
+        psi4 = Psi4Tetrads(Coord_in, enable_rfm_precompute)
+        results_dict = ve.process_dictionary_of_expressions(
+            psi4.__dict__, fixed_mpfs_for_free_symbols=True
+        )
+        ve.compare_or_generate_trusted_results(
+            os.path.abspath(__file__),
+            os.getcwd(),
+            # File basename. If this is set to "trusted_module_test1", then
+            #   trusted results_dict will be stored in tests/trusted_module_test1.py
+            f"{os.path.splitext(os.path.basename(__file__))[0]}_{Coord}",
+            results_dict,
+        )
