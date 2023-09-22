23a85b3b749b43da22b42e74b836c8398e6e7387
zetienne <zachetie@gmail.com>
2023-09-21 21:55:57 -0700

    .github/workflows/main.yml: pip install ipython as nrpylatex depends on it.

--- .github/standalone_bash_script.sh
+++ .github/standalone_bash_script.sh
@@ -8,7 +8,7 @@ python -m pip install --upgrade pip
 if [ -f requirements.txt ]; then
   pip install -r requirements.txt
 fi
-pip install mypy pylint black clang-format
+pip install mypy pylint black clang-format ipython
 
 # Display sympy version
 echo "Running CI tests with SymPy version = $(isympy --version)"
