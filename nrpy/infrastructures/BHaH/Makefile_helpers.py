4bc131dcd7421e5f090eacc58ca99d68cd00dd2d
zetienne <zachetie ** at* gmail>
2023-10-03 15:04:49 -0700

    nrpy/infrastructures/BHaH/Makefile_helpers.py: Ingore flags when 'flags' doesn't exist in cpuinfo.

--- nrpy/infrastructures/BHaH/Makefile_helpers.py
+++ nrpy/infrastructures/BHaH/Makefile_helpers.py
@@ -135,28 +135,29 @@ def output_CFunctions_function_prototypes_and_construct_Makefile(
             # Sometimes cpu_info.get("l2_cache_size") returns a value that has explicit units, like 2.5MiB,
             #  ignore these cases
             pass
-    if any("avx512" in flag for flag in cpu_info["flags"]):
-        # -march=native hangs when using GCC on
-        avx512_features = [
-            "avx512f",
-            "avx512pf",
-            "avx512er",
-            "avx512cd",
-            "avx512vl",
-            "avx512bw",
-            "avx512dq",
-            "avx512ifma",
-            "avx512vbmi",
-        ]
-        # manually add the avx-512 featureset
-        avx512_compileflags = ""
-        for feature in avx512_features:
-            if any(flag.replace("_", "") == feature for flag in cpu_info["flags"]):
-                avx512_compileflags += f"-m{feature} "
-        for key, value in CFLAGS_dict.items():
-            CFLAGS_dict[key] = CFLAGS_dict[key].replace(
-                "-march=native", avx512_compileflags
-            )
+    if "flags" in cpu_info:
+        if any("avx512" in flag for flag in cpu_info["flags"]):
+            # -march=native hangs when using GCC on
+            avx512_features = [
+                "avx512f",
+                "avx512pf",
+                "avx512er",
+                "avx512cd",
+                "avx512vl",
+                "avx512bw",
+                "avx512dq",
+                "avx512ifma",
+                "avx512vbmi",
+            ]
+            # manually add the avx-512 featureset
+            avx512_compileflags = ""
+            for feature in avx512_features:
+                if any(flag.replace("_", "") == feature for flag in cpu_info["flags"]):
+                    avx512_compileflags += f"-m{feature} "
+            for key, value in CFLAGS_dict.items():
+                CFLAGS_dict[key] = CFLAGS_dict[key].replace(
+                    "-march=native", avx512_compileflags
+                )
 
     if CC == "gcc":
         for key in CFLAGS_dict:
