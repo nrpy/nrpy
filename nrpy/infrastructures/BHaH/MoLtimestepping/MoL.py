1a27e0ba5874e848f4e2e2b3c76f1ff52929951c
zachetienne <zachetie@gmail.com>
2023-10-30 21:02:03 -0700

    Various: Fix doctests that were affected by new 150-character linewidth limit.

--- nrpy/infrastructures/BHaH/MoLtimestepping/MoL.py
+++ nrpy/infrastructures/BHaH/MoLtimestepping/MoL.py
@@ -859,7 +859,8 @@ def register_CFunctions(
      * - y_n_gfs are used to store data for the vector of gridfunctions y_i at t_n, at the start of each MoL timestep
      * - non_y_n_gfs are needed for intermediate (e.g., k_i) storage in chosen MoL method
      */
-    void MoL_malloc_non_y_n_gfs(const commondata_struct *restrict commondata, const params_struct *restrict params, MoL_gridfunctions_struct *restrict gridfuncs) {
+    void MoL_malloc_non_y_n_gfs(const commondata_struct *restrict commondata, const params_struct *restrict params,
+                                MoL_gridfunctions_struct *restrict gridfuncs) {
     #include "set_CodeParameters.h"
       const int Nxx_plus_2NGHOSTS_tot = Nxx_plus_2NGHOSTS0 * Nxx_plus_2NGHOSTS1 * Nxx_plus_2NGHOSTS2;
       gridfuncs->y_nplus1_running_total_gfs = (REAL *restrict)malloc(sizeof(REAL) * NUM_EVOL_GFS * Nxx_plus_2NGHOSTS_tot);
