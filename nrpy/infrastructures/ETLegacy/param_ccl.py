a5357431b5051271871af0df8fe33c6b56823b71
Samuel Cupp <scupp1@my.apsu.edu>
2023-12-01 15:29:47 -0800

    ETLegacy: fixed param.ccl parameter duplication

--- nrpy/infrastructures/ETLegacy/param_ccl.py
+++ nrpy/infrastructures/ETLegacy/param_ccl.py
@@ -38,12 +38,14 @@ restricted:
             and CFunction.ET_current_thorn_CodeParams_used
         ):
             for CPname in CFunction.ET_current_thorn_CodeParams_used:
-                CParam = par.glb_code_params_dict[CPname]
-                paramccl_str += f'{CParam.c_type_alias} {CParam.name} "(see NRPy+ for parameter definition)"\n'
-                paramccl_str += "{\n"
-                paramccl_str += ' *:* :: "All values accepted. NRPy+ does not restrict the allowed ranges of parameters yet."\n'
-                paramccl_str += f"}} {CParam.defaultvalue}\n\n"
-                CParams_registered_to_params_ccl += [CPname]
+                # only declare parameters once
+                if CPname not in CParams_registered_to_params_ccl:
+                    CParam = par.glb_code_params_dict[CPname]
+                    paramccl_str += f'{CParam.c_type_alias} {CParam.name} "(see NRPy+ for parameter definition)"\n'
+                    paramccl_str += "{\n"
+                    paramccl_str += ' *:* :: "All values accepted. NRPy+ does not restrict the allowed ranges of parameters yet."\n'
+                    paramccl_str += f"}} {CParam.defaultvalue}\n\n"
+                    CParams_registered_to_params_ccl += [CPname]
     output_Path = Path(project_dir) / thorn_name
     output_Path.mkdir(parents=True, exist_ok=True)
     with open(output_Path / "param.ccl", "w", encoding="utf-8") as file:
