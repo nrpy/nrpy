e279d4e34b6627d239528b37898157fe371bf732
Samuel Cupp <scupp1@my.apsu.edu>
2023-12-11 15:32:39 -0800

    ETLegacy: added aux variable BCs

--- nrpy/infrastructures/ETLegacy/boundary_conditions.py
+++ nrpy/infrastructures/ETLegacy/boundary_conditions.py
@@ -36,8 +36,7 @@ Since we choose NewRad boundary conditions, we must register all
 evolved gridfunctions to have boundary type "none". This is because
 NewRad directly modifies the RHSs.
 
-This code is based on Kranc's McLachlan/ML_BSSN/src/Boundaries.cc code.
-"""
+This code is based on Kranc's McLachlan/ML_BSSN/src/Boundaries.cc code."""
 
     c_type = "void"
     name = f"{thorn_name}_specify_Driver_BoundaryConditions"
@@ -110,8 +109,7 @@ Since we choose NewRad boundary conditions, we must register all
 evolved gridfunctions to have boundary type "none". This is because
 NewRad directly modifies the RHSs.
 
-This code is based on Kranc's McLachlan/ML_BSSN/src/Boundaries.cc code.
-"""
+This code is based on Kranc's McLachlan/ML_BSSN/src/Boundaries.cc code."""
 
     c_type = "void"
     name = f"{thorn_name}_specify_evol_BoundaryConditions"
@@ -140,7 +138,81 @@ schedule FUNC_NAME in MoL_PostStep
 
 schedule GROUP ApplyBCs as {thorn_name}_ApplyBCs in MoL_PostStep after FUNC_NAME
 {{
-}} "Group for applying boundary conditions"
+}} "Apply registered boundary conditions"
+""",
+        ),
+    ]
+    cfc.register_CFunction(
+        subdirectory=thorn_name,
+        includes=includes,
+        desc=desc,
+        c_type=c_type,
+        name=name,
+        params=params,
+        body=body,
+        ET_thorn_name=thorn_name,
+        ET_schedule_bins_entries=ET_schedule_bins_entries,
+    )
+
+
+def register_CFunction_specify_aux_BoundaryConditions(thorn_name: str) -> None:
+    """
+    Register C functions for specifying boundary conditions within a given thorn.
+
+    This function generates and registers C code that sets the boundary conditions
+    for auxiliary grid functions within a specific thorn. The
+    boundary conditions are specified based on the `Boundary` method in the
+    Einstein Toolkit.
+
+    :param thorn_name: The name of the thorn for which to specify boundary conditions.
+    :return: None
+    """
+    includes = [
+        "stdio.h",
+        "cctk.h",
+        "cctk_Arguments.h",
+        "cctk_Parameters.h",
+        "cctk_Faces.h",
+        "util_Table.h",
+    ]
+
+    desc = """
+
+This code is based on Kranc's McLachlan/ML_BSSN/src/Boundaries.cc code."""
+
+    c_type = "void"
+    name = f"{thorn_name}_specify_aux_BoundaryConditions"
+    params = "CCTK_ARGUMENTS"
+
+    body = f"""  DECLARE_CCTK_ARGUMENTS_{name};
+DECLARE_CCTK_PARAMETERS;
+CCTK_INT ierr CCTK_ATTRIBUTE_UNUSED = 0;
+"""
+    for gfname, gf in sorted(gri.glb_gridfcs_dict.items()):
+        if gf.group == "AUX":
+            body += f"""
+ierr = Boundary_SelectVarForBC(cctkGH, CCTK_ALL_FACES, 1, -1, "{thorn_name}::{gfname}GF", "flat");
+if (ierr < 0) CCTK_ERROR("Failed to register BC with Boundary for {thorn_name}::{gfname}GF!");
+"""
+
+    # Baikal has something like
+    #
+    # const CCTK_INT bndsize = FD_order / 2 + 1;  // <- bndsize = number of ghostzones
+    # ierr = Boundary_SelectVarForBC(cctkGH, CCTK_ALL_FACES, bndsize, -1, "Baikal::HGF", "flat");
+
+    ET_schedule_bins_entries = [
+        (
+            "MoL_PseudoEvolution",
+            f"""
+schedule FUNC_NAME in MoL_PseudoEvolution after {thorn_name}_BSSN_constraints
+{{
+  LANG: C
+  SYNC: aux_variables
+}} "Register boundary conditions and perform AMR+interprocessor synchronization"
+
+schedule GROUP ApplyBCs as {thorn_name}_auxgfs_ApplyBCs in MoL_PseudoEvolution after FUNC_NAME
+{{
+}} "Apply registered boundary conditions"
 """,
         ),
     ]
@@ -250,13 +322,13 @@ Set up NewRad boundary conditions.
   Obviously for var_radpower>0, var_at_infinite_r is the value of
     the variable at r->infinity. var_char_speed is the propagation
     speed at the outer boundary, and var_radpower is the radial
-    falloff rate.
-"""
+    falloff rate."""
     c_type = "void"
     name = f"{thorn_name}_specify_NewRad_BoundaryConditions_parameters"
     params = "CCTK_ARGUMENTS"
     body = f"""  DECLARE_CCTK_ARGUMENTS_{name};
   DECLARE_CCTK_PARAMETERS;
+
 """
     for gfname, gf in gri.glb_gridfcs_dict.items():
         if gf.group == "EVOL" and isinstance(gf, gri.ETLegacyGridFunction):
@@ -270,7 +342,7 @@ Set up NewRad boundary conditions.
 schedule FUNC_NAME in MoL_CalcRHS after {thorn_name}_RHS
 {{
   LANG: C
-  READS: evol_variables(everywhere)
+  READS:  evol_variables(everywhere)
   WRITES: evol_variables_rhs(boundary)
 }} "NewRad boundary conditions, scheduled right after RHS eval."
 """,
