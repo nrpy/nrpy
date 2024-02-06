b2cb5dfd217aee91162bd21415397dba79580a6a
zetienne <zachetie@gmail.com>
2024-02-06 12:18:21 -0800

    nrpy/helpers/cse_preprocess_postprocess.py: map_rat_to_sym dictionary can take sp.Rational OR sp.S.NegativeOne as key

--- nrpy/helpers/cse_preprocess_postprocess.py
+++ nrpy/helpers/cse_preprocess_postprocess.py
@@ -94,7 +94,7 @@ def cse_preprocess(
 
     # Maps to hold symbol to rational and rational to symbol mappings
     symbol_to_Rational_dict: Dict[sp.Basic, sp.Rational] = OrderedDict()
-    map_rat_to_sym: Dict[sp.Rational, sp.Basic] = OrderedDict()
+    map_rat_to_sym: Dict[sp.Rational | sp.S.NegativeOne, sp.Basic] = OrderedDict()
 
     # Loop over each expression in the input list
     for i, expr in enumerate(expr_list):
@@ -142,7 +142,7 @@ def cse_preprocess(
                     )
 
                 # Update subexpression in the subtree
-                subtree.expr = repl * sign
+                subtree.expr = cast(sp.Expr, repl * sign)
 
                 if sign < 0:
                     tree.build(subtree, clear=False)
