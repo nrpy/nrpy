eb3914f0845a255ca273b3cfe057479a6dce99f8
Steven R. Brandt <sbrandt@cct.lsu.edu>
2024-02-12 15:25:29 -0600

    Fixed various issues with colored.

--- nrpy/helpers/colored.py
+++ nrpy/helpers/colored.py
@@ -11,7 +11,9 @@ from typing_extensions import Literal
 color_names = Literal["red", "green", "yellow", "blue", "magenta", "cyan"]
 
 
-def not_colored(arg: Any, c: color_names) -> str:  # pylint: disable=unused-argument
+def coloring_is_disabled(
+    arg: Any, c: color_names
+) -> str:  # pylint: disable=unused-argument
     """
     Provide stringified version of the argument with coloring disabled.
 
@@ -19,8 +21,12 @@ def not_colored(arg: Any, c: color_names) -> str:  # pylint: disable=unused-argu
     :param c: a valid color name
 
     :return: a stringified version of arg
+    >>> coloring_is_disabled('fish','blue')
+    'fish'
+    >>> coloring_is_disabled('fish','green')
+    'fish'
     """
-    return repr(arg)
+    return str(arg)
 
 
 colors: Dict[color_names, str] = {
@@ -34,14 +40,21 @@ colors: Dict[color_names, str] = {
 reset = "\033[0m"
 
 
-def colored(arg: Any, c: color_names) -> str:
-    """
+def coloring_is_enabled(arg: Any, c: color_names) -> str:
+    r"""
     Return the stringified version of the argument with the specified color.
+    Coloring will be disabled if the output is not being sent to a notebook cell
+    or a plain old console.
 
     :param arg: The object to convert to a string
     :param c: The name of the color to use
     :return: A stringified and colored version of `arg`
     :raises AssertionError: If `c` is not a string or not a valid color name
+    >>> import re
+    >>> re.sub(r'.\[','ESC',coloring_is_enabled('fish','blue'))
+    'ESC34mfishESC0m'
+    >>> re.sub(r'.\[','ESC',coloring_is_enabled('fish','green'))
+    'ESC32mfishESC0m'
     """
     assert isinstance(c, str), "Color name must be a string"
     assert c in colors, f"Invalid color name: {c}"
@@ -58,7 +71,9 @@ is_jupyter = (
     and type(sys.stdout).__module__ == "ipykernel.iostream"
 )
 if (not is_tty) and (not is_jupyter):
-    colored = not_colored
+    colored = coloring_is_disabled
+else:
+    colored = coloring_is_enabled
 
 if __name__ == "__main__":
     print(colored("red", "red"), colored("green", "green"))
