e85c1cdf5d9e6b2a16c56c8de81190179a6a0a7b
Thiago Assumpção <assumpcaothiago@gmail.com>
2024-02-14 09:50:17 -0500

    nrpy/equations/nrpyelliptic/CommonParams.py: remove cast(), clean up parameter declaration.

--- nrpy/equations/nrpyelliptic/CommonParams.py
+++ nrpy/equations/nrpyelliptic/CommonParams.py
@@ -8,8 +8,6 @@ License: BSD 2-Clause
 """
 
 # Step P1: Import needed NRPy+ core modules:
-from typing import cast, List
-import sympy as sp
 import nrpy.params as par  # NRPy+: Parameter interface
 
 thismodule = __name__
@@ -38,23 +36,11 @@ bare_mass_1 = par.register_CodeParameter(
 zPunc = par.register_CodeParameter("REAL", thismodule, "zPunc", 5.0, commondata=True)
 
 # Step P3.c.1: linear momentum 0
-P0_x = par.register_CodeParameter("REAL", thismodule, "P0_x", 0.0, commondata=True)
-P0_y = par.register_CodeParameter("REAL", thismodule, "P0_y", 0.0, commondata=True)
-P0_z = par.register_CodeParameter("REAL", thismodule, "P0_z", 0.0, commondata=True)
-P0U = cast(List[sp.Expr], [P0_x, P0_y, P0_z])  # define list with linear momenta
+P0U = par.register_CodeParameters("REAL", thismodule, ["P0_x", "P0_y", "P0_z"], 0.0, commondata=True)
 # Step P3.c.2: linear momentum 1
-P1_x = par.register_CodeParameter("REAL", thismodule, "P1_x", 0.0, commondata=True)
-P1_y = par.register_CodeParameter("REAL", thismodule, "P1_y", 0.0, commondata=True)
-P1_z = par.register_CodeParameter("REAL", thismodule, "P1_z", 0.0, commondata=True)
-P1U = cast(List[sp.Expr], [P1_x, P1_y, P1_z])  # define list with linear momenta
+P1U = par.register_CodeParameters("REAL", thismodule, ["P1_x", "P1_y", "P1_z"], 0.0, commondata=True)
 
 # Step P3.d.1: angular momentum 0
-S0_x = par.register_CodeParameter("REAL", thismodule, "S0_x", 0.0, commondata=True)
-S0_y = par.register_CodeParameter("REAL", thismodule, "S0_y", 0.0, commondata=True)
-S0_z = par.register_CodeParameter("REAL", thismodule, "S0_z", 0.0, commondata=True)
-S0U = cast(List[sp.Expr], [S0_x, S0_y, S0_z])  # define list with angular momenta
+S0U = par.register_CodeParameters("REAL", thismodule, ["S0_x", "S0_y", "S0_z"], 0.0, commondata=True)
 # Step P3.d.2: angular momentum 1
-S1_x = par.register_CodeParameter("REAL", thismodule, "S1_x", 0.0, commondata=True)
-S1_y = par.register_CodeParameter("REAL", thismodule, "S1_y", 0.0, commondata=True)
-S1_z = par.register_CodeParameter("REAL", thismodule, "S1_z", 0.0, commondata=True)
-S1U = cast(List[sp.Expr], [S1_x, S1_y, S1_z])  # define list with angular momenta
+S1U = par.register_CodeParameters("REAL", thismodule, ["S1_x", "S1_y", "S1_z"], 0.0, commondata=True)
