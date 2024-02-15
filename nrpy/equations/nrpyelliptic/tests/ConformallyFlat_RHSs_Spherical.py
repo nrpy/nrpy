f88de234aa3ae4df75dbef88cac09ed7e0be7f0a
Thiago Assumpção <assumpcaothiago@gmail.com>
2024-02-15 14:39:36 -0500

    nrpy/equations/nrpyelliptic/tests/ConformallyFlat_RHSs_*.py: add trusted dictionaries for testing.

---
+++ nrpy/equations/nrpyelliptic/tests/ConformallyFlat_RHSs_Spherical.py
@@ -0,0 +1,7 @@
+from mpmath import mpf  # type: ignore
+
+trusted_dict = {
+    "residual": mpf("17.6969974207454092162512767743099"),
+    "uu_rhs": mpf("0.539178464196700523184132417185058"),
+    "vv_rhs": mpf("2.76286323141929118216151651269817"),
+}
