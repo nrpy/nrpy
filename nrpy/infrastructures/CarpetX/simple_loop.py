667803dcc2fe5914bb9bb8e09feba6dab2b62c77
zachetienne <zachetie@gmail.com>
2024-02-24 12:38:39 -0800

    nrpy/infrastructures/CarpetX/simple_loop.py: Correct several issues with formatting, etc.

--- nrpy/infrastructures/CarpetX/simple_loop.py
+++ nrpy/infrastructures/CarpetX/simple_loop.py
@@ -7,48 +7,46 @@ Contributor: Ken Sible
 Email: ksible *at* outlook *dot* com
 """
 
-from typing import List
+from typing import Union, List
 
 
 def simple_loop(
     loop_body: str,
     enable_simd: bool = False,
     loop_region: str = "",
-    loop_centering: List[int] = [0, 0, 0],
+    loop_centering: Union[List[int], None] = None,
     run_on_device: bool = True,
 ) -> str:
     """
     Generate a simple loop in C (for use inside of a function).
 
-    :param loop_body: Loop body
-    :param enable_simd: Enable SIMD support
-    :param loop_region: Loop over all points on a numerical grid or just the interior
-    :param loop_centering: Sets the centering of the loop (0 = "V", 1 = "C")
-    :param run_on_device: Run loop kernel on device (GPU) instead of host (CPU)
-    :return: Complete loop code, output as a string.
+    :param loop_body: The body of the loop to be generated.
+    :param enable_simd: Flag to enable SIMD support. Currently unsupported.
+    :param loop_region: Specifies the loop region (e.g., 'all points', 'interior').
+    :param loop_centering: Specifies the centering of the loop (0 = 'V', 1 = 'C').
+    :param run_on_device: Flag to run loop kernel on device (GPU) instead of host (CPU).
+    :return: The complete loop code as a string.
 
     Doctests:
     >>> from nrpy.helpers.generic import clang_format
     >>> print(clang_format(simple_loop(loop_body='// <INTERIOR>', loop_region="all points")))
-    grid.loop_all_device<0, 0, 0>(
-        grid.nghostzones,
-        [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+    grid.loop_all_device<0, 0, 0>(grid.nghostzones, [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
       // <INTERIOR>
-    }); // END LOOP: grid.loop_all_device<0, 0, 0>
+    }); // END LOOP: grid.loop_all_device<0, 0, 0>(
     <BLANKLINE>
-      >>> print(clang_format(simple_loop(loop_body='// <INTERIOR>', loop_region="interior", run_on_device=False)))
-    grid.loop_int<0, 0, 0>(
-        grid.nghostzones,
-        [=] CCTK_HOST(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+    >>> print(clang_format(simple_loop(loop_body='// <INTERIOR>', loop_region="interior", run_on_device=False)))
+    grid.loop_int<0, 0, 0>(grid.nghostzones, [=] CCTK_HOST(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
       // <INTERIOR>
-    }); // END LOOP: grid.loop_int<0, 0, 0>
+    }); // END LOOP: grid.loop_int<0, 0, 0>(
     <BLANKLINE>
     """
+    if loop_centering is None:
+        loop_centering = [0, 0, 0]
+
     loop_macro = "grid.loop_"
-    # 'AllPoints': loop over all points on a numerical grid, including ghost zones
+
     if loop_region == "all points":
         loop_macro += "all"
-    # 'InteriorPoints': loop over the interior of a numerical grid, i.e. exclude ghost zones
     elif loop_region == "interior":
         loop_macro += "int"
     else:
@@ -67,19 +65,12 @@ def simple_loop(
     else:
         run_on = "HOST"
 
-    if loop_centering[0] not in [0, 1]:
-        raise ValueError(
-            f"loop_centering[0] = {loop_centering[0]} unsupported. Choose 0 or 1 for vertex- or cell-centering, respectively."
-        )
-    if loop_centering[1] not in [0, 1]:
-        raise ValueError(
-            f"loop_centering[1] = {loop_centering[1]} unsupported. Choose 0 or 1 for vertex- or cell-centering, respectively."
-        )
-    if loop_centering[2] not in [0, 1]:
-        raise ValueError(
-            f"loop_centering[2] = {loop_centering[2]} unsupported. Choose 0 or 1 for vertex- or cell-centering, respectively."
-        )
-    # loop_centering: set loop to properly choose stencil based on the given centering
+    for i, center in enumerate(loop_centering):
+        if center not in [0, 1]:
+            raise ValueError(
+                f"loop_centering[{i}] = {center} unsupported. Choose 0 or 1 for vertex- or cell-centering, respectively."
+            )
+
     loop_macro += f"<{loop_centering[0]}, {loop_centering[1]}, {loop_centering[2]}>(\n"
 
     return f"""{loop_macro}grid.nghostzones,
