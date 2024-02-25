122cbd048b7ade0d1aa66bd9c0c741f10acba832
zachetienne <zachetie@gmail.com>
2024-02-24 18:46:45 -0800

    .github/standalone_bash_script.sh: simplify local CI script. .github/workflows/main.yml: simplify a little more.

--- .github/standalone_bash_script.sh
+++ .github/standalone_bash_script.sh
@@ -4,13 +4,12 @@
 rm -rf ~/.cache/nrpy
 
 # Display Python version
-python --version
+python_version=$(python --version)
+echo "Python version: $python_version"
 
 # Install dependencies
 python -m pip install --upgrade pip
-if [ -f requirements.txt ]; then
-  pip install -U -r requirements.txt
-fi
+pip install -U -r requirements.txt
 pip install -U -r requirements-dev.txt
 
 # Display sympy version
@@ -19,8 +18,7 @@ echo "Running CI tests with SymPy version = $(isympy --version)"
 # Initialize array to store failed tests
 failed_tests=()
 
-# Use find to locate python files based on pattern or directory structure.
-#   Don't analyze Python scripts in tests/ (though they should pass!)
+# Use find to locate python files
 python_files=$(find . -name '*.py' -not -name '__init__.py' -not -path './build/*' -not -path '*/tests/*')
 
 # Loop through each python file
@@ -29,16 +27,16 @@ for python_file in $python_files; do
   echo "-={ $python_file }=-"
   echo "-={ Step 1: Doctests/run Python module }=-"
   PYTHONPATH=.:$PYTHONPATH DOCTEST_MODE=1 python $python_file || { failed_tests+=("doctest in $python_file"); break; }
-  if [ "$(python --version 2>&1 | cut -d ' ' -f2)" != "3.6.7" ]; then
-    # Turns out that black in Python 3.6.7 has a heart attack when parsing equations/general_relativity/BSSN_quantities.py:
-    # INTERNAL ERROR: Black produced code that is not equivalent to the source. Please report a bug on .... [HOW ABOUT NO. BEGGING FOR WONTFIX]
+
+  if [[ "$python_version" != *"3.6.7"* ]]; then
     echo "-={ Step 2: black }=-"
     black --check $python_file || { failed_tests+=("black in $python_file"); break; }
     echo "-={ Step 3: mypy }=-"
     PYTHONPATH=.:$PYTHONPATH mypy --strict --pretty --allow-untyped-calls $python_file || { failed_tests+=("mypy in $python_file"); break; }
   fi
+
   echo "-={ Step 4: pylint }=-"
-  PYTHONPATH=.:$PYTHONPATH pylint_score=$(pylint --rcfile=.pylintrc $python_file | tail -2 | grep -Eo '[0-9\.]+' | head -1 || echo "0")
+  pylint_score=$(PYTHONPATH=.:$PYTHONPATH pylint --rcfile=.pylintrc $python_file | tail -2 | grep -Eo '[0-9\.]+' | head -1 || echo "0")
   echo "Pylint score is $pylint_score"
   if (( $(echo "$pylint_score < 9.91" | bc -l) )); then
     PYTHONPATH=.:$PYTHONPATH pylint --rcfile=.pylintrc $python_file || true
@@ -46,10 +44,8 @@ for python_file in $python_files; do
     failed_tests+=("pylint in $python_file")
     break
   fi
-  if (( $(echo "$pylint_score < 10.00" | bc -l) )); then
-    PYTHONPATH=.:$PYTHONPATH pylint --rcfile=.pylintrc $python_file || true
-  fi
-  echo "-={ Step 5: pydocstyle }=-"  # Added pydocstyle step
+
+  echo "-={ Step 5: pydocstyle }=-"
   pydocstyle $python_file || { failed_tests+=("pydocstyle in $python_file"); break; }
 done
 
