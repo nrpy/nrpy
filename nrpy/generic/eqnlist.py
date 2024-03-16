2a29fc592a1cc27ce0d8e7de0356bc4a4b4a8f9c
Steven R. Brandt <sbrandt@cct.lsu.edu>
2024-03-15 21:06:31 -0500

    Gettin eqnlist to work

--- nrpy/generic/eqnlist.py
+++ nrpy/generic/eqnlist.py
@@ -5,6 +5,9 @@ from sympy.core.expr import Expr
 from sympy import symbols, Function, diff
 from sympy import cse as cse_ 
 from here import here
+from colored import colored
+
+from nrpy.generic.sympywrap import *
 
 # Quieting mypy. Not sure if this trick is needed.
 cse_return = Tuple[List[Tuple[Symbol,Expr]],List[Expr]]
@@ -32,6 +35,7 @@ class EqnList:
         self.inputs:Set[Symbol] = set()
         self.outputs:Set[Symbol] = set()
         self.order:List[Symbol] = list()
+        self.verbose = True
 
     def add_param(self, lhs:Symbol)->None:
         assert lhs not in self.outputs, f"The symbol '{lhs}' is alredy in outputs"
@@ -49,67 +53,120 @@ class EqnList:
         self.outputs.add(lhs)
 
     def add_eqn(self, lhs:Symbol, rhs:Expr)->None:
+        assert lhs not in self.eqns, f"Equation for '{lhs}' is already defined"
         self.eqns[lhs] = rhs
 
     def diagnose(self)->None:
         """ Discover inconsistencies and errors in the param/input/output/equation sets. """
         needed:Set[Symbol] = set()
-        used:Set[Symbol] = set()
+        complete:Dict[Symbol,int] = dict()
         temps:Set[Symbol] = set()
         self.order = list()
 
         read:Set[Symbol] = set()
         written:Set[Symbol] = set()
 
+        if self.verbose:
+            print(colored("Inputs:","green"),self.inputs)
+            print(colored("Outputs:","green"),self.outputs)
+            print(colored("Eqns:","green"))
+            for lhs in self.eqns:
+                print(" ",colored(lhs,"cyan"),"=",self.eqns[lhs])
+
         for k in self.eqns:
             written.add(k)
             for q in self.eqns[k].free_symbols:
                 read.add(q)
 
+        if self.verbose:
+            print(colored("Read:","green"),read)
+            print(colored("Written:","green"),written)
+            print(colored("Temps:","green"),temps)
+
+        for k in self.inputs:
+            assert k in read, f"Symbol '{k}' is in inputs, but it is never read."
+            assert k not in written, f"Symbol '{k}' is in inputs, but it is assigned to."
+
+        for k in self.outputs:
+            assert k in written, f"Symbol '{k}' is in outputs, but it is never written"
+
         for k in written:
-            if k in read:
+            if k not in self.outputs:
                 temps.add(k)
-            else:
-                assert k in self.outputs, f"Symbol '{k}' is written, but it is not a temp and not in outputs"
+
+        for k in read:
+            if k not in self.inputs:
+                temps.add(k)
+
+        for k in temps:
+            assert k in read, f"Temmporary variable '{k}' is never read"
+            assert k in written, f"Temporary variable '{k}' is never written"
 
         for k in read:
             assert k in self.inputs or self.params or temps, f"Symbol '{k}' is read, but it is not a temp, parameter, or input."
 
         for k in self.outputs:
+            # The outputs are all needed
+            # for a successful computation
             needed.add(k)
-            assert k in self.eqns, f"The symbol '{k}' appears in outputs, but no eqn assigns to it."
-        again = True
-        wait = False
-        while again:
-            again = False
-            for k,v in enumerate(self.eqns):
-                assert k not in self.inputs, f"The symbol '{k}' appears in inputs, but it is assigned to by '{k} = {v}'"
-                assert k not in self.params, f"The symbol '{k}' appears in params, but it is assigned to by '{k} = {v}'"
-                if k in needed and k not in used:
-                    if k in temps or k in self.outputs:
-                        wait = False
-                        for k2 in v.free_symbols:
-                            if k2 in temps and k2 not in used:
-                                wait = True
-                                if k2 not in needed:
-                                    needed.add(cast(Symbol,k2))
-                                    again = True
-                    if wait:
-                        continue
-                    used.add(k)
-                    self.order.append(k)
-                    for k2 in v.free_symbols:
-                        k3 = cast(Symbol, k2)
-                        needed.add(k3)
-                        assert k3 not in self.outputs, f"The symbol '{k3}' appears in outputs, but it is read by '{k} = {v}'"
-                    again = True
-        assert not wait, "Cycle in temp assignment"
-        for k in needed:
-            assert k in self.inputs or k in self.params or k in self.eqns,\
-                f"The symbol '{k}' is needed but is not defined"
         for k in self.inputs:
-            if k not in needed:
-                print(f"Warning: symbol '{k}' appears in inputs but is not needed")
+            complete[k] = 0
+
+        again = True
+        generation = 0
+        find_cycle : Symbol = None
+        while True:
+            if not again and len(needed) > len(complete) and find_cycle is not None:
+                again = True
+                # The symbol in find_cycle is not complete
+                # but we assert that it is to help us find
+                # symbols that are part of a cycle.
+                complete[find_cycle] = generation
+            find_cycle = None
+            if not again:
+                break
+            generation += 1
+            again = False
+            for k in list(needed):
+                if k in self.inputs and k not in complete:
+                    complete[k] = generation
+                    again = True
+                elif k not in complete:
+                    assert k in self.eqns, f"Symbol '{k}' is needed but is not written"
+                    v = self.eqns[k]
+                    can_add = True
+                    for k2 in v.free_symbols:
+                       if k2 not in complete:
+                           # A variable is only complete
+                           # if all its free symbols are
+                           # complete
+                           can_add = False
+                       if k2 not in needed:
+                           # Since k2 is needed to assign
+                           # a value to k, k2 is also needed
+                           needed.add(cast(Symbol,k2))
+                           # more work to do
+                           again = True
+                    if can_add:
+                        complete[k] = generation
+                        self.order.append(k)
+                        again = True
+                    else:
+                        find_cycle = k
+        print(colored("Order:","green"),self.order)
+        for k,v in self.eqns.items():
+            assert k in complete, f"Eqn '{k} = {v}' does not contribute to the output."
+            val1 = complete[k]
+            for k2 in v.free_symbols:
+                val2 = complete[k2]
+                assert val1 >= val2, f"Symbol '{k}' is part of an assignment cycle."
+        for k in needed:
+            if k not in complete:
+                print(f"Symbol '{k}' needed but could not be evaluated. Cycle in assignment?")
+        for k in self.inputs:
+            assert k in complete, f"Symbol '{k}' appears in inputs but is not complete"
+        for k in self.eqns:
+            assert k in complete, f"Equation '{k} = {self.eqns[k]}' is never complete"
 
     def trim(self)->None:
         """ Remove temporaries of the form "a=b". They are clutter. """
@@ -153,19 +210,58 @@ class EqnList:
             print(" ",k,"->",self.eqns[k])
 
 if __name__ == "__main__":
-    a, b, c, d, e, f, g = symbols("a b c d e f g")
-    el = EqnList()
-    el.add_input(a)
-    el.add_input(f)
-    el.add_input(b)
-    el.add_output(d)
-    el.add_eqn(c, e+b+a**3)
-    el.add_eqn(c, g+f+a**3)
-    el.add_eqn(e, a**2)
-    el.add_eqn(g, e)
-    el.add_eqn(d, c*b+a**3)
-    el.diagnose()
-    el.dump()
-    el.cse()
-    el.diagnose()
-    el.dump()
+    a, b, c, d, e, f, g, q, r = symbols("a b c d e f g q r")
+
+    try:
+        el = EqnList()
+        el.add_eqn(r, q) # cycle
+        el.add_eqn(q, r) # cycle
+        el.add_eqn(a, r)
+        el.add_output(a)
+        el.diagnose()
+    except AssertionError as ae:
+        assert "cycle" in str(ae)
+        print("Test one passed")
+        print()
+
+    try:
+        el = EqnList()
+        el.add_input(a)
+        el.add_input(f)
+        el.add_input(b)
+        el.add_output(d)
+        el.add_eqn(c, g+f+a**3 + q)
+        el.add_eqn(e, a**2)
+        el.add_eqn(g, e)
+        el.add_eqn(d, c*b+a**3)
+        el.diagnose()
+    except AssertionError as ae:
+        assert "'q' is never written" in str(ae)
+        print("Test two passed")
+        print()
+
+    try:
+        el = EqnList()
+        el.add_input(a)
+        el.add_input(f)
+        el.add_output(d)
+        el.add_eqn(a, f+2)
+        el.add_eqn(d, a)
+        el.diagnose()
+    except AssertionError as ae:
+        assert "Symbol 'a' is in inputs, but it is assigned to." in str(ae)
+        print("Test three passed")
+        print()
+
+    try:
+        el = EqnList()
+        el.add_input(a)
+        el.add_input(f)
+        el.add_output(d)
+        el.add_output(e)
+        el.add_eqn(d, a+f)
+        el.diagnose()
+    except AssertionError as ae:
+        assert "Symbol 'e' is in outputs, but it is never written" in str(ae)
+        print("Test four passed")
+        print()
