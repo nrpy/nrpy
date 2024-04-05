1c9fb3c91d701ab324788957a262cce2eb564c06
Nishita Jadoo <nishita@localhost.localdomain>
2024-04-04 22:22:15 -0700

    add superB.h

--- setup.py
+++ setup.py
@@ -107,6 +107,9 @@ if __name__ == "__main__":
                 "TP_utilities.h",
                 "TwoPunctures.h",
             ],
+            "nrpy.infrastructures.superB.superB": [
+                "superB.h",
+            ],
             "nrpy.helpers": ["simd_intrinsics.h"],
             "nrpy": ["py.typed"],
         },
