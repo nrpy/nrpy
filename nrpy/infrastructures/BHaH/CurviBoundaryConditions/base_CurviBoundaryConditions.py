11907c558444cd4ad7cf4ffbb50be435c718418c
s.tootle <sdtootle@gmail.com>
2024-04-16 12:30:44 -0700

    Remove old unused code

--- nrpy/infrastructures/BHaH/CurviBoundaryConditions/base_CurviBoundaryConditions.py
+++ nrpy/infrastructures/BHaH/CurviBoundaryConditions/base_CurviBoundaryConditions.py
@@ -584,22 +584,7 @@ class base_register_CFunction_apply_bcs_inner_only:
         self.cfunc_type = "void"
         self.name = "apply_bcs_inner_only"
         self.params = "const commondata_struct *restrict commondata, const params_struct *restrict params, const bc_struct *restrict bcstruct, REAL *restrict gfs"
-
-    body = r"""
-  // Unpack bc_info from bcstruct
-  const bc_info_struct *bc_info = &bcstruct->bc_info;
-
-  // collapse(2) results in a nice speedup here, esp in 2D. Two_BHs_collide goes from
-  //    5550 M/hr to 7264 M/hr on a Ryzen 9 5950X running on all 16 cores with core affinity.
-#pragma omp parallel for collapse(2)  // spawn threads and distribute across them
-  for(int which_gf=0;which_gf<NUM_EVOL_GFS;which_gf++) {
-    for(int pt=0;pt<bc_info->num_inner_boundary_points;pt++) {
-      const int dstpt = bcstruct->inner_bc_array[pt].dstpt;
-      const int srcpt = bcstruct->inner_bc_array[pt].srcpt;
-      gfs[IDX4pt(which_gf, dstpt)] = bcstruct->inner_bc_array[pt].parity[evol_gf_parity[which_gf]] * gfs[IDX4pt(which_gf, srcpt)];
-    } // END for(int pt=0;pt<num_inner_pts;pt++)
-  } // END for(int which_gf=0;which_gf<NUM_EVOL_GFS;which_gf++)
-"""
+        self.body = ""
 
 
 ###############################
