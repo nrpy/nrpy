651b66e490fccb85f3fd6968e7dd9f1c8395ec9e
Nishita <njadoo@ncsu.edu>
2024-04-25 19:04:10 -0700

    fix pylint issues

--- nrpy/examples/superB_two_blackholes_collide.py
+++ nrpy/examples/superB_two_blackholes_collide.py
@@ -31,9 +31,6 @@ import nrpy.infrastructures.BHaH.general_relativity.BSSN_C_codegen_library as BC
 import nrpy.infrastructures.BHaH.CodeParameters as CPs
 import nrpy.infrastructures.BHaH.CurviBoundaryConditions.CurviBoundaryConditions as cbc
 import nrpy.infrastructures.BHaH.cmdline_input_and_parfiles as cmdpar
-import nrpy.infrastructures.BHaH.Makefile_helpers as Makefile
-from nrpy.infrastructures.BHaH.MoLtimestepping import MoL
-import nrpy.infrastructures.BHaH.main_c as main
 import nrpy.infrastructures.BHaH.numerical_grids_and_timestep as numericalgrids
 import nrpy.infrastructures.BHaH.diagnostics.progress_indicator as progress
 from nrpy.infrastructures.BHaH import rfm_precompute
