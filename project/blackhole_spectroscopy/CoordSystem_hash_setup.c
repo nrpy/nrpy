5b9af7a93e12560b3ed6fe9d74af788c9bc2ad24
Nishita <njadoo@ncsu.edu>
2024-04-29 08:44:45 -0700

    Delete project/*

--- project/blackhole_spectroscopy/CoordSystem_hash_setup.c
+++
@@ -1,9 +0,0 @@
-#include "BHaH_defines.h"
-#include "BHaH_function_prototypes.h"
-/*
- * Set up CoordSystem_hash for all grids
- */
-void CoordSystem_hash_setup(commondata_struct *restrict commondata, griddata_struct *restrict griddata) {
-  // Set CoordSystem_hash, used for multi-coordinate-system evolutions. Hash is #define'd in BHaH_defines.h
-  griddata[0].params.CoordSystem_hash = SINHSPHERICAL;
-}
