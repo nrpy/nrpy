86c93010b28354e317f9e10574184e0dfd45d682
zetienne <zachetie@gmail.com>
2024-05-20 09:28:42 -0700

    nrpy/equations/nrpyelliptic/CommonParams.py: Do not define time in NRPyElliptic CommonParams; this is a reserved parameter for MoL, and unused in NRPyElliptic anyway.

--- nrpy/equations/nrpyelliptic/CommonParams.py
+++ nrpy/equations/nrpyelliptic/CommonParams.py
@@ -12,22 +12,19 @@ import nrpy.params as par  # NRPy+: Parameter interface
 
 # Parameters common to/needed by all NRPyElliptic Python modules
 
-# Step P1: Define time parameter
-time = par.register_CodeParameter("REAL", __name__, "time", 0.0)
-
-# Step P2.a: Define the damping parameter used in the hyperbolic relaxation method
+# Step P1.a: Define the damping parameter used in the hyperbolic relaxation method
 eta_damping = par.register_CodeParameter(
     "REAL", __name__, "eta_damping", 10.0, commondata=True
 )
 
-# Step P2.b: Define the parameter that controls minimum value of wavespeed in the numerical domain
+# Step P1.b: Define the parameter that controls minimum value of wavespeed in the numerical domain
 MINIMUM_GLOBAL_WAVESPEED = par.register_CodeParameter(
     "REAL", __name__, "MINIMUM_GLOBAL_WAVESPEED", 0.7, commondata=True
 )
 
-# Step P3: Define puncture parameters
+# Step P2: Define puncture parameters
 
-# Step P3.a: bare masses
+# Step P2.a: bare masses
 bare_mass_0 = par.register_CodeParameter(
     "REAL", __name__, "bare_mass_0", 0.0, commondata=True
 )
@@ -35,23 +32,23 @@ bare_mass_1 = par.register_CodeParameter(
     "REAL", __name__, "bare_mass_1", 0.0, commondata=True
 )
 
-# Step P3.b: position of the punctures in the z axis
+# Step P2.b: position of the punctures in the z axis
 zPunc = par.register_CodeParameter("REAL", __name__, "zPunc", 5.0, commondata=True)
 
-# Step P3.c.1: linear momentum 0
+# Step P2.c.1: linear momentum 0
 P0U = par.register_CodeParameters(
     "REAL", __name__, ["P0_x", "P0_y", "P0_z"], 0.0, commondata=True
 )
-# Step P3.c.2: linear momentum 1
+# Step P2.c.2: linear momentum 1
 P1U = par.register_CodeParameters(
     "REAL", __name__, ["P1_x", "P1_y", "P1_z"], 0.0, commondata=True
 )
 
-# Step P3.d.1: angular momentum 0
+# Step P2.d.1: angular momentum 0
 S0U = par.register_CodeParameters(
     "REAL", __name__, ["S0_x", "S0_y", "S0_z"], 0.0, commondata=True
 )
-# Step P3.d.2: angular momentum 1
+# Step P2.d.2: angular momentum 1
 S1U = par.register_CodeParameters(
     "REAL", __name__, ["S1_x", "S1_y", "S1_z"], 0.0, commondata=True
 )
