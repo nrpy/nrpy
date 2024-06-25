1146cd516eb4b3461cb28d4e6595cbda70e17889
s.tootle <sdtootle@gmail.com>
2024-06-25 13:30:51 -0700

    Resolve module issues seobnr

--- nrpy/examples/seobnrv5_aligned_spin_inspiral.py
+++ nrpy/examples/seobnrv5_aligned_spin_inspiral.py
@@ -13,9 +13,9 @@ import os
 import shutil
 
 import nrpy.c_function as cfc
-import nrpy.infrastructures.BHaH.BHaH_defines_h as Bdefines_h
 import nrpy.infrastructures.BHaH.cmdline_input_and_parfiles as cmdpar
 import nrpy.infrastructures.BHaH.CodeParameters as CPs
+import nrpy.infrastructures.BHaH.header_definitions.openmp.output_BHaH_defines_h as Bdefines_h
 import nrpy.infrastructures.BHaH.Makefile_helpers as Makefile
 import nrpy.infrastructures.BHaH.seobnr.SEOBNR_C_codegen_library as seobnr_CCL
 import nrpy.params as par
