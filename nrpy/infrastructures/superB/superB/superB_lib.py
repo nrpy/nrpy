0df1d12343fc3654acc20d0364f5e62c88996639
Zachariah B. Etienne <zachetie@gmail.com>
2024-07-21 22:03:58 -0700

    nrpy/infrastructures/superB/superB/superB_lib.py: Make compatible with all Python versions now that setuptools has a separate typing package, which we use.

--- nrpy/infrastructures/superB/superB/superB_lib.py
+++ nrpy/infrastructures/superB/superB/superB_lib.py
@@ -6,36 +6,44 @@ Author: Nishita Jadoo
 """
 
 import shutil
+import sys
 from pathlib import Path
 
 
 def copy_superB_header_files(superB_Path: Path) -> None:
     """
-    Copy superB.h.
+    Copy superB.h into the specified directory.
 
     :param superB_Path: The path of the directory where the files will be copied.
+    :raises ImportError: If the importlib.resources module is not found, indicating
+                         a Python version earlier than 3.7 where this module was introduced,
+                         or if pkg_resources is not available.
     """
     superB_Path.mkdir(parents=True, exist_ok=True)
 
     try:
-        # only Python 3.7+ has importlib.resources
-        from importlib import resources  # pylint: disable=E1101,C0415
+        # Only Python 3.9+ has importlib.resources.files()
+        if sys.version_info >= (3, 9):
+            import importlib.resources  # pylint: disable=E1101,C0415
+            from importlib.abc import Traversable  # pylint: disable=C0415,W4904
 
-        for header_file in ["superB.h"]:
-            source_path = (
-                # pylint: disable=E1101
-                resources.files("nrpy.infrastructures.superB.superB")
-                / header_file
-            )
-            shutil.copy(str(source_path), str(superB_Path / header_file))
-    except ImportError:  # Fallback to resource_filename for older Python versions
-        # pylint: disable=E1101,C0415
-        from pkg_resources import resource_filename  # type: ignore
+            for header_file in ["superB.h"]:
+                source_path: Traversable = (
+                    importlib.resources.files("nrpy.infrastructures.superB.superB")
+                    / header_file
+                )
+                shutil.copy(str(source_path), str(superB_Path / header_file))
+        else:
+            raise ImportError("Using fallback for importlib.resources")
+    except (ImportError, AttributeError):
+        # Fallback for versions without importlib.resources or importlib.resources.files()
+        try:
+            from pkg_resources import resource_filename  # pylint: disable=C0415
 
-        for header_file in ["superB.h"]:
-            source_path = resource_filename(
-                "nrpy.infrastructures.superB.superB",
-                header_file,
-            )
-            print("Source path:", source_path)
-            shutil.copy(source_path, str(superB_Path))  # type: ignore
+            for header_file in ["superB.h"]:
+                source_path_str: str = resource_filename(
+                    "nrpy.infrastructures.superB.superB", header_file
+                )
+                shutil.copy(source_path_str, str(superB_Path / header_file))
+        except ImportError as e:
+            raise ImportError("pkg_resources is not available") from e
