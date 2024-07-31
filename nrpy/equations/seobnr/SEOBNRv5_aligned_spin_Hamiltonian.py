0c3abc149662a209f8ef4d7b3a43ea8f7d2bdb88
siddharth-mahesh <sm0193@mix.wvu.edu>
2024-07-31 09:44:54 -0400

    ensure all floating point numbers in Hamiltonian are infinite precision

--- nrpy/equations/seobnr/SEOBNRv5_aligned_spin_Hamiltonian.py
+++ nrpy/equations/seobnr/SEOBNRv5_aligned_spin_Hamiltonian.py
@@ -8,6 +8,7 @@ License: BSD 2-Clause
 
 # Step P1: Import needed modules:
 import sympy as sp
+from mpmath import mpf
 
 # The name of this module ("WaveEquation") is given by __name__:
 thismodule = __name__
@@ -27,10 +28,7 @@ class SEOBNRv5_aligned_spin_Hamiltonian_quantities:
         calculations.
 
         Inputs: 'm1', 'm2', 'r', 'prstar', 'pphi', 'chi1', 'chi2', 'a6', and 'dSO'
-        Outputs: 'xi', 'Hreal',
-                 'dHreal_dr', 'dHreal_dprstar', 'dHreal_dpphi',
-                 'dHreal_dr_dr', 'dHreal_dr_dpphi' ,
-                 'dHreal_dr_circ' , and 'dHreal_dpphi_circ'
+        Outputs: 'xi' and 'Hreal'
         """
         m1, m2, self.r, self.prstar, self.pphi, chi1, chi2, a6, dSO = sp.symbols(
             "m1 m2 r prstar pphi chi1 chi2 a6 dSO", real=True
@@ -91,120 +89,132 @@ class SEOBNRv5_aligned_spin_Hamiltonian_quantities:
         am = (m1 * chi1 - m2 * chi2) / M
         Qnos = (
             (
-                0.121954868780449 * self.nu * self.prstar**8 / self.r
+                mpf(0.121954868780449) * self.nu * self.prstar**8 / self.r
                 + self.prstar**6
-                * (6.0 * self.nu**3 - 5.4 * self.nu**2 - 2.78300763695006 * self.nu)
+                * (
+                    mpf(6.0) * self.nu**3
+                    - mpf(5.4) * self.nu**2
+                    - mpf(2.78300763695006) * self.nu
+                )
                 / self.r**2
                 + self.prstar**4
-                * (10.0 * self.nu**3 - 131.0 * self.nu**2 + 92.7110442849544 * self.nu)
+                * (
+                    mpf(10.0) * self.nu**3
+                    - mpf(131.0) * self.nu**2
+                    + mpf(92.7110442849544) * self.nu
+                )
                 / self.r**3
             )
             + (
                 self.prstar**8
                 * (
-                    -6.0 * self.nu**4
-                    + 3.42857142857143 * self.nu**3
-                    + 3.33842023648322 * self.nu**2
-                    + 1.38977750996128 * self.nu
+                    -mpf(6.0) * self.nu**4
+                    + mpf(3.42857142857143) * self.nu**3
+                    + mpf(3.33842023648322) * self.nu**2
+                    + mpf(1.38977750996128) * self.nu
                 )
                 / self.r**2
                 + self.prstar**6
                 * (
-                    -14.0 * self.nu**4
-                    + 188.0 * self.nu**3
-                    - 89.5298327361234 * self.nu**2
-                    - 33.9782122170436 * self.nu
+                    -mpf(14.0) * self.nu**4
+                    + mpf(188.0) * self.nu**3
+                    - mpf(89.5298327361234) * self.nu**2
+                    - mpf(33.9782122170436) * self.nu
                 )
                 / self.r**3
                 + self.prstar**4
                 * (
-                    602.318540416564 * self.nu**3
-                    + self.nu**2 * (118.4 * sp.log(self.r) - 1796.13660498019)
-                    + self.nu * (452.542166996693 - 51.6952380952381 * sp.log(self.r))
+                    mpf(602.318540416564) * self.nu**3
+                    + self.nu**2 * (mpf(118.4) * sp.log(self.r) - mpf(1796.13660498019))
+                    + self.nu
+                    * (mpf(452.542166996693) - mpf(51.6952380952381) * sp.log(self.r))
                 )
                 / self.r**4
             )
             + (
-                1.48275342024365 * self.nu * self.prstar**8 / self.r**2.5
-                - 11.3175085791863 * self.nu * self.prstar**6 / self.r**3.5
-                + 147.443752990146 * self.nu * self.prstar**4 / self.r**4.5
+                mpf(1.48275342024365) * self.nu * self.prstar**8 / self.r**2.5
+                - mpf(11.3175085791863) * self.nu * self.prstar**6 / self.r**3.5
+                + mpf(147.443752990146) * self.nu * self.prstar**4 / self.r**4.5
             )
-            + self.prstar**4 * (-6.0 * self.nu**2 + 8.0 * self.nu) / self.r**2
+            + self.prstar**4 * (-mpf(6.0) * self.nu**2 + mpf(8.0) * self.nu) / self.r**2
         )
         d5 = 0
         Dnons = (
             self.r
             * (
-                6730497718123.02 * self.nu**3
-                + 22295347200.0 * self.nu**2 * d5
-                + 133772083200.0 * self.nu**2 * self.r**2
-                + 1822680546449.21 * self.nu**2 * self.r
-                + 80059249540278.2 * self.nu**2
-                + 22295347200.0 * self.nu * d5 * self.r
-                - 193226342400.0 * self.nu * d5
-                + 2589101062873.81 * self.nu * self.r**2
-                + 10611661054566.2 * self.nu * self.r
-                - 12049908701745.2 * self.nu
-                + 5107745331375.71 * self.r**2
-                - 326837426.241486 * self.r * (14700.0 * self.nu + 42911.0)
-                - 39476764256925.6 * self.r
+                mpf(6730497718123.02) * self.nu**3
+                + mpf(22295347200.0) * self.nu**2 * d5
+                + mpf(133772083200.0) * self.nu**2 * self.r**2
+                + mpf(1822680546449.21) * self.nu**2 * self.r
+                + mpf(80059249540278.2) * self.nu**2
+                + mpf(22295347200.0) * self.nu * d5 * self.r
+                - mpf(193226342400.0) * self.nu * d5
+                + mpf(2589101062873.81) * self.nu * self.r**2
+                + mpf(10611661054566.2) * self.nu * self.r
+                - mpf(12049908701745.2) * self.nu
+                + mpf(5107745331375.71) * self.r**2
+                - mpf(326837426.241486) * self.r * (14700.0 * self.nu + 42911.0)
+                - mpf(39476764256925.6) * self.r
                 - (
-                    -5041721180160.0 * self.nu**2
-                    - 25392914995744.3 * self.nu
-                    - 879923036160.0 * self.r**2
-                    - 283115520.0 * self.r * (14700.0 * self.nu + 42911.0)
-                    + 104186110149937.0
+                    -mpf(5041721180160.0) * self.nu**2
+                    - mpf(25392914995744.3) * self.nu
+                    - mpf(879923036160.0) * self.r**2
+                    - mpf(283115520.0) * self.r * (14700.0 * self.nu + 42911.0)
+                    + mpf(104186110149937.0)
                 )
                 * sp.log(self.r)
-                + 5787938193408.0 * sp.log(self.r) ** 2
-                + 275059053208689.0
+                + mpf(5787938193408.0) * sp.log(self.r) ** 2
+                + mpf(275059053208689.0)
             )
             / (
-                55296.0
+                mpf(55296.0)
                 * self.nu
                 * (
-                    14515200.0 * self.nu**3
-                    - 42636451.6032331 * self.nu**2
-                    - 7680.0 * self.nu * (315.0 * d5 + 890888.810272497)
-                    + 4331361844.61149 * self.nu
-                    + 1002013764.01019
+                    mpf(14515200.0) * self.nu**3
+                    - mpf(42636451.6032331) * self.nu**2
+                    - mpf(7680.0) * self.nu * (315.0 * d5 + mpf(890888.810272497))
+                    + mpf(4331361844.61149) * self.nu
+                    + mpf(1002013764.01019)
                 )
-                - 967680.0
+                - mpf(967680.0)
                 * self.r**3
                 * (
-                    -138240.0 * self.nu**2
-                    - 2675575.66847905 * self.nu
-                    - 5278341.3229329
+                    -mpf(138240.0) * self.nu**2
+                    - mpf(2675575.66847905) * self.nu
+                    - mpf(5278341.3229329)
                 )
-                - 9216.0
+                - mpf(9216.0)
                 * self.r**2
                 * (
-                    -197773496.793534 * self.nu**2
-                    - 7680.0 * self.nu * (315.0 * d5 + 405152.309729121)
-                    + 2481453539.84635 * self.nu
-                    + 5805304367.87913
+                    -mpf(197773496.793534) * self.nu**2
+                    - mpf(7680.0) * self.nu * (315.0 * d5 + mpf(405152.309729121))
+                    + mpf(2481453539.84635) * self.nu
+                    + mpf(5805304367.87913)
                 )
                 + self.r
                 * (
-                    5927865218923.02 * self.nu**3
-                    + 70778880.0 * self.nu**2 * (315.0 * d5 + 2561145.80918574)
-                    - 138141470005001.0 * self.nu**2
-                    - 4718592.0 * self.nu * (40950.0 * d5 + 86207832.4415642)
-                    + 450172889755120.0 * self.nu
-                    + 86618264430493.3 * (1 - 0.496948781616935 * self.nu) ** 2
-                    + 188440788778196.0
+                    mpf(5927865218923.02) * self.nu**3
+                    + mpf(70778880.0)
+                    * self.nu**2
+                    * (315.0 * d5 + mpf(2561145.80918574))
+                    - mpf(138141470005001.0) * self.nu**2
+                    - mpf(4718592.0) * self.nu * (40950.0 * d5 + mpf(86207832.4415642))
+                    + mpf(450172889755120.0) * self.nu
+                    + mpf(86618264430493.3)
+                    * (1 - mpf(0.496948781616935) * self.nu) ** 2
+                    + mpf(188440788778196.0)
                 )
-                + 5787938193408.0 * self.r * sp.log(self.r) ** 2
+                + mpf(5787938193408.0) * self.r * sp.log(self.r) ** 2
                 + (
-                    -1698693120.0 * self.nu * (11592.0 * self.nu + 69847.0)
-                    + 879923036160.0 * self.r**3
-                    + 283115520.0 * self.r**2 * (14700.0 * self.nu + 42911.0)
-                    + 49152.0
+                    -mpf(1698693120.0) * self.nu * (11592.0 * self.nu + 69847.0)
+                    + mpf(879923036160.0) * self.r**3
+                    + mpf(283115520.0) * self.r**2 * (14700.0 * self.nu + 42911.0)
+                    + mpf(49152.0)
                     * self.r
                     * (
-                        102574080.0 * self.nu**2
-                        + 409207698.136075 * self.nu
-                        - 2119671837.36038
+                        mpf(102574080.0) * self.nu**2
+                        + mpf(409207698.136075) * self.nu
+                        - mpf(2119671837.36038)
                     )
                 )
                 * sp.log(self.r)
@@ -214,8 +224,8 @@ class SEOBNRv5_aligned_spin_Hamiltonian_quantities:
             7680.0
             * self.r**4
             * (
-                -5416406.59541186 * self.nu**2
-                + 28.0 * self.nu * (1920.0 * a6 + 733955.307463037)
+                -mpf(5416406.59541186) * self.nu**2
+                + 28.0 * self.nu * (1920.0 * a6 + mpf(733955.307463037))
                 + 2048.0
                 * self.nu
                 * (756.0 * self.nu + 336.0 * self.r + 407.0)
@@ -223,32 +233,32 @@ class SEOBNRv5_aligned_spin_Hamiltonian_quantities:
                 - 7.0
                 * self.r
                 * (
-                    -185763.092693281 * self.nu**2
-                    + 938918.400156317 * self.nu
+                    -mpf(185763.092693281) * self.nu**2
+                    + mpf(938918.400156317) * self.nu
                     - 245760.0
                 )
                 - 3440640.0
             )
             / (
-                241555486248.807 * self.nu**4
+                mpf(241555486248.807) * self.nu**4
                 + 1120.0
                 * self.nu**3
                 * (
-                    -17833256.898555 * self.r**2
-                    - 163683964.822551 * self.r
-                    - 1188987459.03162
+                    -mpf(17833256.898555) * self.r**2
+                    - mpf(163683964.822551) * self.r
+                    - mpf(1188987459.03162)
                 )
                 + 7.0
                 * self.nu**2
                 * (
                     -39321600.0 * a6 * (3.0 * self.r + 59.0)
-                    + 745857848.115604 * a6
-                    + 1426660551.8844 * self.r**5
-                    - 3089250703.76879 * self.r**4
-                    - 6178501407.53758 * self.r**3
-                    + 2064783811.32587 * self.r**2
-                    + 122635399361.987 * self.r
-                    + 276057889687.011
+                    + mpf(745857848.115604) * a6
+                    + mpf(1426660551.8844) * self.r**5
+                    - mpf(3089250703.76879) * self.r**4
+                    - mpf(6178501407.53758) * self.r**3
+                    + mpf(2064783811.32587) * self.r**2
+                    + mpf(122635399361.987) * self.r
+                    + mpf(276057889687.011)
                 )
                 + 67645734912.0 * self.nu**2 * sp.log(self.r) ** 2
                 + 53760.0
@@ -266,13 +276,13 @@ class SEOBNRv5_aligned_spin_Hamiltonian_quantities:
                     + 128.0
                     * self.r
                     * (
-                        -6852.34813868015 * self.r**4
-                        + 4264.6962773603 * self.r**3
-                        + 8529.39255472061 * self.r**2
-                        + 13218.7851094412 * self.r
-                        - 33722.4297811176
+                        -mpf(6852.34813868015) * self.r**4
+                        + mpf(4264.6962773603) * self.r**3
+                        + mpf(8529.39255472061) * self.r**2
+                        + mpf(13218.7851094412) * self.r
+                        - mpf(33722.4297811176)
                     )
-                    + 113485.217444961
+                    + mpf(113485.217444961)
                     * self.r
                     * (
                         -self.r**4
@@ -281,7 +291,7 @@ class SEOBNRv5_aligned_spin_Hamiltonian_quantities:
                         + 8.0 * self.r
                         + 16.0
                     )
-                    + 148.04406601634
+                    + mpf(148.04406601634)
                     * self.r
                     * (
                         349.0 * self.r**4
@@ -294,15 +304,15 @@ class SEOBNRv5_aligned_spin_Hamiltonian_quantities:
                 + 32768.0
                 * self.nu
                 * (
-                    -1882456.23663972 * self.nu**2
-                    - 38842241.4769507 * self.nu
-                    + 161280.0 * self.r**5
+                    -mpf(1882456.23663972) * self.nu**2
+                    - mpf(38842241.4769507) * self.nu
+                    + mpf(161280.0) * self.r**5
                     + 480.0 * self.r**4 * (756.0 * self.nu + 1079.0)
                     + 960.0 * self.r**3 * (756.0 * self.nu + 1079.0)
                     + 1920.0 * self.r**2 * (588.0 * self.nu + 1079.0)
                     + 240.0
                     * self.r
-                    * (-3024.0 * self.nu**2 - 7466.27061066206 * self.nu + 17264.0)
+                    * (-3024.0 * self.nu**2 - mpf(7466.27061066206) * self.nu + 17264.0)
                     + 13447680.0
                 )
                 * sp.log(self.r)
@@ -377,7 +387,8 @@ class SEOBNRv5_aligned_spin_Hamiltonian_quantities:
         )
         Heff = Hodd + Heven
         self.Hreal = sp.sqrt(1 + 2 * self.nu * (Heff - 1))
-        # compute the derivatives
+
+        # define the derivatives
         self.dHreal_dr = sp.diff(self.Hreal, self.r) / self.nu
         self.dHreal_dprstar = sp.diff(self.Hreal, self.prstar) / self.nu
         self.dHreal_dpphi = sp.diff(self.Hreal, self.pphi) / self.nu
