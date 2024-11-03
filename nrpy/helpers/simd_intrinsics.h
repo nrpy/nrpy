4c3be237e5a9163acc8cb118934d88adf42b9e97
zachetienne <zachetie@gmail.com>
2024-11-02 18:00:59 -0700

    nrpy/helpers/simd_intrinsics.h: Apply insights from https://www.ed-yang.com/worknotes/docs/manual-vectorization/sumreduce/

--- nrpy/helpers/simd_intrinsics.h
+++ nrpy/helpers/simd_intrinsics.h
@@ -104,14 +104,13 @@
 
 // Initialize vector to zero (output is REAL_SIMD_ARRAY)
 #define SetZeroSIMD _mm256_setzero_pd()
-// Horizontal addition (output is a double)
-#define HorizAddSIMD(vec_sum)                                                                                                                        \
+// Horizontal addition (output is a double). Inpsired by https://www.ed-yang.com/worknotes/docs/manual-vectorization/sumreduce/
+#define HorizAddSIMD(vec)                                                                                                                            \
   ({                                                                                                                                                 \
-    __m128d low = _mm256_castpd256_pd128(vec_sum);    /* Extract the lower 128 bits */                                                               \
-    __m128d high = _mm256_extractf128_pd(vec_sum, 1); /* Extract the upper 128 bits */                                                               \
-    __m128d sum_128 = _mm_add_pd(low, high);          /* Add lower and upper parts */                                                                \
-    sum_128 = _mm_hadd_pd(sum_128, sum_128);          /* Perform horizontal addition */                                                              \
-    _mm_cvtsd_f64(sum_128);                           /* Extract final scalar sum */                                                                 \
+    __m128d low = _mm256_castpd256_pd128(vec);                                 /* Extract lower 128 bits */                                          \
+    __m128d high = _mm256_extractf128_pd(vec, 1);                              /* Extract upper 128 bits */                                          \
+    __m128d sum_128 = _mm_add_pd(low, high);                                   /* Add low and high parts */                                          \
+    _mm_cvtsd_f64(sum_128) + _mm_cvtsd_f64(_mm_unpackhi_pd(sum_128, sum_128)); /* Final scalar sum */                                                \
   })
 
 // If compiled with SSE2 SIMD instructions enabled:
