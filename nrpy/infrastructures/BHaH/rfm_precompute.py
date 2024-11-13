0201633b0a6cf55bfe11361164f779bd4befcb0a
s.tootle <sdtootle@gmail.com>
2024-11-13 10:11:44 -0800

    Add multigrid support in rfm_precompute. Promote many variables to be class variables to allow for inheritence.

--- nrpy/infrastructures/BHaH/rfm_precompute.py
+++ nrpy/infrastructures/BHaH/rfm_precompute.py
@@ -5,6 +5,7 @@ Author: Zachariah B. Etienne
         zachetie **at** gmail **dot* com
 """
 
+import os
 from typing import List
 
 import sympy as sp
@@ -27,7 +28,7 @@ class ReferenceMetricPrecompute:
     """
 
     def __init__(self, CoordSystem: str, fp_type: str = "double"):
-        rfm = refmetric.reference_metric[CoordSystem + "_rfm_precompute"]
+        self.rfm = refmetric.reference_metric[CoordSystem + "_rfm_precompute"]
         # Step 7: Construct needed C code for declaring rfmstruct, allocating storage for
         #         rfmstruct arrays, defining each element in each array, reading the
         #         rfmstruct data from memory (both with and without SIMD enabled), and
@@ -48,20 +49,23 @@ class ReferenceMetricPrecompute:
         # Sort freevars_uniq_vals and freevars_uniq_xx_indep, according to alphabetized freevars_uniq_xx_indep.
         #    Without this step, the ordering of elements in rfmstruct would be random, and would change each time
         #    this function was called.
-        freevars_uniq_xx_indep: List[sp.Expr] = []
-        freevars_uniq_vals: List[sp.Expr] = []
-        if len(rfm.freevars_uniq_xx_indep) > 0:
-            freevars_uniq_xx_indep, freevars_uniq_vals = (
+        self.freevars_uniq_xx_indep: List[sp.Expr] = []
+        self.freevars_uniq_vals: List[sp.Expr] = []
+        if len(self.rfm.freevars_uniq_xx_indep) > 0:
+            self.freevars_uniq_xx_indep, self.freevars_uniq_vals = (
                 list(x)
                 for x in zip(
                     *sorted(
-                        zip(rfm.freevars_uniq_xx_indep, rfm.freevars_uniq_vals), key=str
+                        zip(
+                            self.rfm.freevars_uniq_xx_indep, self.rfm.freevars_uniq_vals
+                        ),
+                        key=str,
                     )
                 )
             )
 
         # Tease out how many variables each function in freevars_uniq_vals
-        Nxx_plus_2NGHOSTS = [
+        self.Nxx_plus_2NGHOSTS = [
             sp.Symbol("Nxx_plus_2NGHOSTS0", real=True),
             sp.Symbol("Nxx_plus_2NGHOSTS1", real=True),
             sp.Symbol("Nxx_plus_2NGHOSTS2", real=True),
@@ -69,84 +73,84 @@ class ReferenceMetricPrecompute:
         which_freevar: int = 0
         fp_ccg_type = ccg.fp_type_to_sympy_type[fp_type]
         sp_type_alias = {sp_ast.real: fp_ccg_type}
-        for expr in freevars_uniq_vals:
-            if "_of_xx" in str(freevars_uniq_xx_indep[which_freevar]):
+        for expr in self.freevars_uniq_vals:
+            if "_of_xx" in str(self.freevars_uniq_xx_indep[which_freevar]):
                 frees = list(expr.free_symbols)
                 frees_uniq = superfast_uniq(frees)
                 xx_list: List[sp.Basic] = []
                 malloc_size: int = 1
                 for i in range(3):
-                    if rfm.xx[i] in frees_uniq:
-                        xx_list.append(rfm.xx[i])
-                        malloc_size *= Nxx_plus_2NGHOSTS[i]
+                    if self.rfm.xx[i] in frees_uniq:
+                        xx_list.append(self.rfm.xx[i])
+                        malloc_size *= self.Nxx_plus_2NGHOSTS[i]
 
                 self.BHaH_defines_list += [
-                    f"REAL *restrict {freevars_uniq_xx_indep[which_freevar]};"
+                    f"REAL *restrict {self.freevars_uniq_xx_indep[which_freevar]};"
                 ]
-                self.rfm_struct__malloc += f"rfmstruct->{freevars_uniq_xx_indep[which_freevar]} = (REAL *)malloc(sizeof(REAL)*{malloc_size});\n"
+                self.rfm_struct__malloc += f"rfmstruct->{self.freevars_uniq_xx_indep[which_freevar]} = (REAL *)malloc(sizeof(REAL)*{malloc_size});\n"
                 self.rfm_struct__freemem += (
-                    f"free(rfmstruct->{freevars_uniq_xx_indep[which_freevar]});\n"
+                    f"free(rfmstruct->{self.freevars_uniq_xx_indep[which_freevar]});\n"
                 )
                 output_define_and_readvr = False
                 for dirn in range(3):
                     if (
-                        (rfm.xx[dirn] in frees_uniq)
-                        and not (rfm.xx[(dirn + 1) % 3] in frees_uniq)
-                        and not (rfm.xx[(dirn + 2) % 3] in frees_uniq)
+                        (self.rfm.xx[dirn] in frees_uniq)
+                        and not (self.rfm.xx[(dirn + 1) % 3] in frees_uniq)
+                        and not (self.rfm.xx[(dirn + 2) % 3] in frees_uniq)
                     ):
                         self.rfm_struct__define += (
                             f"for(int i{dirn}=0;i{dirn}<Nxx_plus_2NGHOSTS{dirn};i{dirn}++) {{\n"
                             f"  const REAL xx{dirn} = xx[{dirn}][i{dirn}];\n"
-                            f"  rfmstruct->{freevars_uniq_xx_indep[which_freevar]}[i{dirn}] = {sp.ccode(freevars_uniq_vals[which_freevar], type_aliases=sp_type_alias)};\n"
+                            f"  rfmstruct->{self.freevars_uniq_xx_indep[which_freevar]}[i{dirn}] = {sp.ccode(self.freevars_uniq_vals[which_freevar], type_aliases=sp_type_alias)};\n"
                         )
                         self.rfm_struct__define += "}\n\n"
                         self.readvr_str[
                             dirn
-                        ] += f"const REAL {freevars_uniq_xx_indep[which_freevar]} = rfmstruct->{freevars_uniq_xx_indep[which_freevar]}[i{dirn}];\n"
+                        ] += f"const REAL {self.freevars_uniq_xx_indep[which_freevar]} = rfmstruct->{self.freevars_uniq_xx_indep[which_freevar]}[i{dirn}];\n"
                         self.readvr_SIMD_outer_str[
                             dirn
-                        ] += f"const double NOSIMD{freevars_uniq_xx_indep[which_freevar]} = rfmstruct->{freevars_uniq_xx_indep[which_freevar]}[i{dirn}]; "
+                        ] += f"const double NOSIMD{self.freevars_uniq_xx_indep[which_freevar]} = rfmstruct->{self.freevars_uniq_xx_indep[which_freevar]}[i{dirn}]; "
                         self.readvr_SIMD_outer_str[
                             dirn
-                        ] += f"const REAL_SIMD_ARRAY {freevars_uniq_xx_indep[which_freevar]} = ConstSIMD(NOSIMD{freevars_uniq_xx_indep[which_freevar]});\n"
+                        ] += f"const REAL_SIMD_ARRAY {self.freevars_uniq_xx_indep[which_freevar]} = ConstSIMD(NOSIMD{self.freevars_uniq_xx_indep[which_freevar]});\n"
                         self.readvr_SIMD_inner_str[
                             dirn
-                        ] += f"const REAL_SIMD_ARRAY {freevars_uniq_xx_indep[which_freevar]} = ReadSIMD(&rfmstruct->{freevars_uniq_xx_indep[which_freevar]}[i{dirn}]);\n"
+                        ] += f"const REAL_SIMD_ARRAY {self.freevars_uniq_xx_indep[which_freevar]} = ReadSIMD(&rfmstruct->{self.freevars_uniq_xx_indep[which_freevar]}[i{dirn}]);\n"
                         output_define_and_readvr = True
 
                 if (
                     (not output_define_and_readvr)
-                    and (rfm.xx[0] in frees_uniq)
-                    and (rfm.xx[1] in frees_uniq)
+                    and (self.rfm.xx[0] in frees_uniq)
+                    and (self.rfm.xx[1] in frees_uniq)
                 ):
                     self.rfm_struct__define += f"""
                 for(int i1=0;i1<Nxx_plus_2NGHOSTS1;i1++) for(int i0=0;i0<Nxx_plus_2NGHOSTS0;i0++) {{
                   const REAL xx0 = xx[0][i0];
                   const REAL xx1 = xx[1][i1];
-                  rfmstruct->{freevars_uniq_xx_indep[which_freevar]}[i0 + Nxx_plus_2NGHOSTS0*i1] = {sp.ccode(freevars_uniq_vals[which_freevar], type_aliases=sp_type_alias)};
+                  rfmstruct->{self.freevars_uniq_xx_indep[which_freevar]}[i0 + Nxx_plus_2NGHOSTS0*i1] = {sp.ccode(self.freevars_uniq_vals[which_freevar], type_aliases=sp_type_alias)};
                 }}\n\n"""
                     self.readvr_str[
                         0
-                    ] += f"const REAL {freevars_uniq_xx_indep[which_freevar]} = rfmstruct->{freevars_uniq_xx_indep[which_freevar]}[i0 + Nxx_plus_2NGHOSTS0*i1];\n"
+                    ] += f"const REAL {self.freevars_uniq_xx_indep[which_freevar]} = rfmstruct->{self.freevars_uniq_xx_indep[which_freevar]}[i0 + Nxx_plus_2NGHOSTS0*i1];\n"
                     self.readvr_SIMD_outer_str[
                         0
-                    ] += f"const double NOSIMD{freevars_uniq_xx_indep[which_freevar]} = rfmstruct->{freevars_uniq_xx_indep[which_freevar]}[i0 + Nxx_plus_2NGHOSTS0*i1]; "
+                    ] += f"const double NOSIMD{self.freevars_uniq_xx_indep[which_freevar]} = rfmstruct->{self.freevars_uniq_xx_indep[which_freevar]}[i0 + Nxx_plus_2NGHOSTS0*i1]; "
                     self.readvr_SIMD_outer_str[
                         0
-                    ] += f"const REAL_SIMD_ARRAY {freevars_uniq_xx_indep[which_freevar]} = ConstSIMD(NOSIMD{freevars_uniq_xx_indep[which_freevar]});\n"
+                    ] += f"const REAL_SIMD_ARRAY {self.freevars_uniq_xx_indep[which_freevar]} = ConstSIMD(NOSIMD{self.freevars_uniq_xx_indep[which_freevar]});\n"
                     self.readvr_SIMD_inner_str[
                         0
-                    ] += f"const REAL_SIMD_ARRAY {freevars_uniq_xx_indep[which_freevar]} = ReadSIMD(&rfmstruct->{freevars_uniq_xx_indep[which_freevar]}[i0 + Nxx_plus_2NGHOSTS0*i1]);\n"
+                    ] += f"const REAL_SIMD_ARRAY {self.freevars_uniq_xx_indep[which_freevar]} = ReadSIMD(&rfmstruct->{self.freevars_uniq_xx_indep[which_freevar]}[i0 + Nxx_plus_2NGHOSTS0*i1]);\n"
                     output_define_and_readvr = True
 
                 if not output_define_and_readvr:
                     raise RuntimeError(
-                        f"ERROR: Could not figure out the (xx0,xx1,xx2) dependency within the expression for {freevars_uniq_xx_indep[which_freevar]}: {freevars_uniq_vals[which_freevar]}"
+                        f"ERROR: Could not figure out the (xx0,xx1,xx2) dependency within the expression for {self.freevars_uniq_xx_indep[which_freevar]}: {self.freevars_uniq_vals[which_freevar]}"
                     )
 
                 if not output_define_and_readvr:
                     raise RuntimeError(
-                        f"ERROR: Could not figure out the (xx0,xx1,xx2) dependency within the expression for {freevars_uniq_xx_indep[which_freevar]}: {freevars_uniq_vals[which_freevar]}"
+                        f"ERROR: Could not figure out the (xx0,xx1,xx2) dependency within the expression for {self.freevars_uniq_xx_indep[which_freevar]}: {self.freevars_uniq_vals[which_freevar]}"
                     )
 
             which_freevar += 1
@@ -184,16 +188,20 @@ def register_CFunctions_rfm_precompute(
             body += func[1]
 
             combined_BHaH_defines_list.extend(rfm_precompute.BHaH_defines_list)
-            cfc.register_CFunction(
-                includes=includes,
-                desc=desc,
-                cfunc_type=cfunc_type,
-                CoordSystem_for_wrapper_func=CoordSystem,
-                name=name,
-                params=params,
-                include_CodeParameters_h=include_CodeParameters_h,
-                body=body,
+            _, actual_name = cfc.function_name_and_subdir_with_CoordSystem(
+                os.path.join("."), name, CoordSystem
             )
+            if actual_name not in cfc.CFunction_dict:
+                cfc.register_CFunction(
+                    includes=includes,
+                    desc=desc,
+                    cfunc_type=cfunc_type,
+                    CoordSystem_for_wrapper_func=CoordSystem,
+                    name=name,
+                    params=params,
+                    include_CodeParameters_h=include_CodeParameters_h,
+                    body=body,
+                )
 
     BHaH_defines = "typedef struct __rfmstruct__ {\n"
     BHaH_defines += "\n".join(sorted(superfast_uniq(combined_BHaH_defines_list)))
