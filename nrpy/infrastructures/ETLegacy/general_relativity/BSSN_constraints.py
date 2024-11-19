63b5e0d3a0be5c38c6fb9a9d901c8ea3582c93f0
s.tootle <sdtootle@gmail.com>
2024-11-19 11:34:07 -0800

    Revert back to simd for ETK infrastructure

--- nrpy/infrastructures/ETLegacy/general_relativity/BSSN_constraints.py
+++ nrpy/infrastructures/ETLegacy/general_relativity/BSSN_constraints.py
@@ -55,7 +55,7 @@ def register_CFunction_BSSN_constraints(
 
     includes = define_standard_includes()
     if enable_simd:
-        includes += [("./intrinsics/simd_intrinsics.h")]
+        includes += [("./simd/simd_intrinsics.h")]
     desc = r"""Evaluate BSSN constraints."""
     name = f"{thorn_name}_BSSN_constraints_order_{fd_order}"
     body = f"""  DECLARE_CCTK_ARGUMENTS_{name};
