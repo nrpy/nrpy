1138c0e4888bac98f75793b66c0f460e33e90eda
Samuel Tootle <sdtootle@gmail.com>
2025-02-10 12:30:15 -0800

    Remove unnecessary code for non-CUDA codegen

--- nrpy/infrastructures/BHaH/tests/rfm_precompute_defines__openmp__Cylindrical.c
+++ nrpy/infrastructures/BHaH/tests/rfm_precompute_defines__openmp__Cylindrical.c
@@ -17,12 +17,7 @@ static void rfm_precompute_defines__f0_of_xx0_host(const params_struct *restrict
 void rfm_precompute_defines__rfm__Cylindrical(const commondata_struct *restrict commondata, const params_struct *restrict params,
                                               rfm_struct *restrict rfmstruct, REAL *restrict xx[3]) {
   MAYBE_UNUSED const REAL *restrict x0 = xx[0];
-  MAYBE_UNUSED const int Nxx_plus_2NGHOSTS0 = params->Nxx_plus_2NGHOSTS0;
   MAYBE_UNUSED const REAL *restrict x1 = xx[1];
-  MAYBE_UNUSED const int Nxx_plus_2NGHOSTS1 = params->Nxx_plus_2NGHOSTS1;
   MAYBE_UNUSED const REAL *restrict x2 = xx[2];
-  MAYBE_UNUSED const int Nxx_plus_2NGHOSTS2 = params->Nxx_plus_2NGHOSTS2;
-  {
-    rfm_precompute_defines__f0_of_xx0_host(params, rfmstruct, x0);
-  }
+  rfm_precompute_defines__f0_of_xx0_host(params, rfmstruct, x0);
 } // END FUNCTION rfm_precompute_defines__rfm__Cylindrical
--- nrpy/infrastructures/BHaH/tests/rfm_precompute_defines__openmp__HoleySinhSpherical.c
+++ nrpy/infrastructures/BHaH/tests/rfm_precompute_defines__openmp__HoleySinhSpherical.c
@@ -98,30 +98,13 @@ static void rfm_precompute_defines__f1_of_xx1__DD11_host(const params_struct *re
 void rfm_precompute_defines__rfm__HoleySinhSpherical(const commondata_struct *restrict commondata, const params_struct *restrict params,
                                                      rfm_struct *restrict rfmstruct, REAL *restrict xx[3]) {
   MAYBE_UNUSED const REAL *restrict x0 = xx[0];
-  MAYBE_UNUSED const int Nxx_plus_2NGHOSTS0 = params->Nxx_plus_2NGHOSTS0;
   MAYBE_UNUSED const REAL *restrict x1 = xx[1];
-  MAYBE_UNUSED const int Nxx_plus_2NGHOSTS1 = params->Nxx_plus_2NGHOSTS1;
   MAYBE_UNUSED const REAL *restrict x2 = xx[2];
-  MAYBE_UNUSED const int Nxx_plus_2NGHOSTS2 = params->Nxx_plus_2NGHOSTS2;
-  {
-    rfm_precompute_defines__f0_of_xx0_host(params, rfmstruct, x0);
-  }
-  {
-    rfm_precompute_defines__f0_of_xx0__D0_host(params, rfmstruct, x0);
-  }
-  {
-    rfm_precompute_defines__f0_of_xx0__DD00_host(params, rfmstruct, x0);
-  }
-  {
-    rfm_precompute_defines__f0_of_xx0__DDD000_host(params, rfmstruct, x0);
-  }
-  {
-    rfm_precompute_defines__f1_of_xx1_host(params, rfmstruct, x1);
-  }
-  {
-    rfm_precompute_defines__f1_of_xx1__D1_host(params, rfmstruct, x1);
-  }
-  {
-    rfm_precompute_defines__f1_of_xx1__DD11_host(params, rfmstruct, x1);
-  }
+  rfm_precompute_defines__f0_of_xx0_host(params, rfmstruct, x0);
+  rfm_precompute_defines__f0_of_xx0__D0_host(params, rfmstruct, x0);
+  rfm_precompute_defines__f0_of_xx0__DD00_host(params, rfmstruct, x0);
+  rfm_precompute_defines__f0_of_xx0__DDD000_host(params, rfmstruct, x0);
+  rfm_precompute_defines__f1_of_xx1_host(params, rfmstruct, x1);
+  rfm_precompute_defines__f1_of_xx1__D1_host(params, rfmstruct, x1);
+  rfm_precompute_defines__f1_of_xx1__DD11_host(params, rfmstruct, x1);
 } // END FUNCTION rfm_precompute_defines__rfm__HoleySinhSpherical
