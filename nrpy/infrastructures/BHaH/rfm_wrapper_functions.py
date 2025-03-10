9ce6a0ec4af03abc1fb36a9c667c60f64dee2613
zachetienne <zachetie@gmail.com>
2025-03-09 19:45:25 -0700

    nrpy/infrastructures/BHaH/rfm_wrapper_functions.py: sort the rfm_wrapper_function #defines in BHaH_defines.h

--- nrpy/infrastructures/BHaH/rfm_wrapper_functions.py
+++ nrpy/infrastructures/BHaH/rfm_wrapper_functions.py
@@ -112,7 +112,9 @@ def register_CFunctions_CoordSystem_wrapper_funcs() -> None:
         )
 
     BHd_str = ""
-    for key, item in CoordSystem_hash_dict.items():
+    for key, item in sorted(
+        CoordSystem_hash_dict.items(), key=lambda kv: kv[0].upper()
+    ):
         BHd_str += f"#define {key.upper()} {item}\n"
     register_BHaH_defines(__name__, BHd_str)
 
