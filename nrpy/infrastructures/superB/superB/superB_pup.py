4d5e00f85478402b4ac9db4eaf75af84013f8ab2
Nishita <njadoo@ncsu.edu>
2025-03-25 14:09:07 -0700

    fix

--- nrpy/infrastructures/superB/superB/superB_pup.py
+++ nrpy/infrastructures/superB/superB/superB_pup.py
@@ -246,7 +246,7 @@ void pup_MoL_gridfunctions_struct(PUP::er &p, MoL_gridfunctions_struct &gridfunc
         )
         # Don't malloc a zero-sized array.
         if num_gfs == "NUM_AUXEVOL_GFS":
-            prefunc += "  if(NUM_AUXEVOL_GFS > 0) "
+            prefunc += "  //if(NUM_AUXEVOL_GFS > 0)\n"
         # Only y_n gfs need to be saved for superB projects for now
         if gridfunctions == "y_n_gfs":
             prefunc += (
