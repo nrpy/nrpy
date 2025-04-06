90d16aed3978e0e3daa66a6be7f25f34d9d7014e
Samuel Tootle <sdtootle@gmail.com>
2025-04-05 22:38:58 -0700

    Remove not sorting

--- nrpy/helpers/generic.py
+++ nrpy/helpers/generic.py
@@ -18,7 +18,7 @@ from typing import Any, List, Optional, cast
 from nrpy.helpers.cached_functions import is_cached, read_cached, write_cached
 
 default_clang_format_options = (
-    "-style={BasedOnStyle: LLVM, ColumnLimit: 150, SortIncludes: Never}"
+    "-style={BasedOnStyle: LLVM, ColumnLimit: 150}"
 )
 
 
