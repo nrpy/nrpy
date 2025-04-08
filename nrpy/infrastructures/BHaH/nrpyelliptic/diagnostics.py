6068b56290e8909139949097d58a1a2e3137d484
Samuel Tootle <sdtootle@gmail.com>
2025-04-08 14:45:18 -0700

    Refactor nell diagnostics to be cuda aware

--- nrpy/infrastructures/BHaH/nrpyelliptic/diagnostics.py
+++ nrpy/infrastructures/BHaH/nrpyelliptic/diagnostics.py
@@ -219,12 +219,18 @@ def register_CFunction_diagnostics(
         commondata=True,
     )
 
+    parallelization = par.parval_from_str("parallelization")
     includes = ["BHaH_defines.h", "BHaH_function_prototypes.h"]
     desc = "Diagnostics."
     cfunc_type = "void"
     name = "diagnostics"
     params = (
         "commondata_struct *restrict commondata, griddata_struct *restrict griddata"
+    )
+    params += (
+        ", griddata_struct *restrict griddata_host"
+        if parallelization in ["cuda"]
+        else ""
     )
 
     # fmt: off
@@ -254,8 +260,12 @@ def register_CFunction_diagnostics(
     body = r"""  // Output progress to stderr
   progress_indicator(commondata, griddata);
 
-  // Since this version of NRPyElliptic is unigrid, we simply set the grid index to 0
-  const int grid = 0;
+  // Grid data output
+  const int n_step = commondata->nn, outevery = commondata->diagnostics_output_every;
+  const REAL time = commondata->time;
+
+  REAL global_norm = -1e9;
+  for(int grid = 0; grid < commondata->NUMGRIDS; ++grid) {
 
   // Set gridfunctions aliases
   REAL *restrict y_n_gfs = griddata[grid].gridfuncs.y_n_gfs;
@@ -264,6 +274,15 @@ def register_CFunction_diagnostics(
   // Set params
   params_struct *restrict params = &griddata[grid].params;
 #include "set_CodeParameters.h"
+"""
+    if parallelization in ["cuda"]:
+        body += r"""
+  REAL *restrict host_y_n_gfs = griddata_host[grid].gridfuncs.y_n_gfs;
+  REAL *restrict host_diag_gfs = griddata_host[grid].gridfuncs.diagnostic_output_gfs;
+  if (n_step % outevery == 0) {
+    size_t streamid = params->grid_idx % NUM_STREAMS;
+    cpyDevicetoHost__gf(commondata, params, host_y_n_gfs, y_n_gfs, UUGF, UUGF, streamid);
+  }
 """
     if enable_rfm_precompute:
         body += r"""  // Set rfm_struct
@@ -283,36 +302,51 @@ def register_CFunction_diagnostics(
   compute_residual_all_points(commondata, params, xx, auxevol_gfs, y_n_gfs, diagnostic_output_gfs);
 """
 
+    if parallelization in ["cuda"]:
+        body += r"""if (n_step % outevery == 0) {
+    BHAH_DEVICE_SYNC();
+    size_t streamid = params->grid_idx % NUM_STREAMS;
+    cpyDevicetoHost__gf(commondata, params, host_diag_gfs, diagnostic_output_gfs, RESIDUAL_HGF, RESIDUAL_HGF, streamid);
+  }"""
+
     body += r"""
   // Set integration radius for l2-norm computation
   const REAL integration_radius = 1000;
 
   // Compute l2-norm of Hamiltonian constraint violation
-  const REAL residual_H = compute_L2_norm_of_gridfunction(commondata, griddata, integration_radius, RESIDUAL_HGF, diagnostic_output_gfs);
+  const REAL residual_H = compute_L2_norm_of_gridfunction(commondata, &griddata[grid], integration_radius, RESIDUAL_HGF, diagnostic_output_gfs);
+  global_norm = MAX(global_norm, residual_H);
+  } // END for(grid=0; grid<commondata->NUMGRIDS; ++grid)
 
   // Update residual to be used in stop condition
-  commondata->log10_current_residual = residual_H;
+  commondata->log10_current_residual = global_norm;
 
   // Output l2-norm of Hamiltonian constraint violation to file
   {
     char filename[256];
     sprintf(filename, "residual_l2_norm.txt");
-    FILE *outfile = (nn == 0) ? fopen(filename, "w") : fopen(filename, "a");
+    FILE *outfile = (n_step == 0) ? fopen(filename, "w") : fopen(filename, "a");
     if (!outfile) {
       fprintf(stderr, "Error: Cannot open file %s for writing.\n", filename);
       exit(1);
     }
-    fprintf(outfile, "%6d %10.4e %.17e\n", nn, time, residual_H);
+    fprintf(outfile, "%6d %10.4e %.17e\n", n_step, time, global_norm);
     fclose(outfile);
   }
 
-  // Grid data output
-  const int n_step = commondata->nn, outevery = commondata->diagnostics_output_every;
   if (n_step % outevery == 0) {
-    // Set reference metric grid xx
+    // Only consider a single grid for now.
+    const int grid = 0;
+    params_struct *restrict params = &griddata[grid].params;
+"""
+
+    body += r"""// Set reference metric grid xx
     REAL *restrict xx[3];
     for (int ww = 0; ww < 3; ww++)
         xx[ww] = griddata[grid].xx[ww];
+
+    // Sync with device when relevant
+    BHAH_DEVICE_SYNC();
 
     // 1D output
     diagnostics_nearest_1d_y_axis(commondata, params, xx, &griddata[grid].gridfuncs);
@@ -322,7 +356,9 @@ def register_CFunction_diagnostics(
     diagnostics_nearest_2d_xy_plane(commondata, params, xx, &griddata[grid].gridfuncs);
     diagnostics_nearest_2d_yz_plane(commondata, params, xx, &griddata[grid].gridfuncs);
   }
-"""
+""".replace(
+        "griddata", "griddata_host" if parallelization in ["cuda"] else "griddata"
+    )
     if enable_progress_indicator:
         body += "progress_indicator(commondata, griddata);"
     body += r"""
