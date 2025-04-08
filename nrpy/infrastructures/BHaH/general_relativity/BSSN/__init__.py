02f4168beafc065d2814e9b1956b7431104dd471
zachetienne <zachetie@gmail.com>
2025-04-08 14:12:02 -0700

    nrpy/infrastructures/BHaH/general_relativity/BSSN/__init__.py: Blacken

--- nrpy/infrastructures/BHaH/general_relativity/BSSN/__init__.py
+++ nrpy/infrastructures/BHaH/general_relativity/BSSN/__init__.py
@@ -1,1 +1,10 @@
-from . import initial_data, rhs_eval, diagnostics, constraints, cahdprefactor_gf, enforce_detgammabar_equals_detgammahat, psi4_decomposition, Ricci_eval
+from . import (
+    initial_data,
+    rhs_eval,
+    diagnostics,
+    constraints,
+    cahdprefactor_gf,
+    enforce_detgammabar_equals_detgammahat,
+    psi4_decomposition,
+    Ricci_eval,
+)
