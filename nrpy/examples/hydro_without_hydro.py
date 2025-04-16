cfbc64138d903b681f3b4a6e8d3290c594ca5ffd
Samuel Tootle <sdtootle@gmail.com>
2025-04-16 13:20:35 -0700

    Make hydro without hydro cuda compatible

--- nrpy/examples/hydro_without_hydro.py
+++ nrpy/examples/hydro_without_hydro.py
@@ -9,6 +9,8 @@ Author: Leonardo Rosa Werneck
         wernecklr **at** gmail **dot* com
 """
 
+import argparse
+
 #########################################################
 # STEP 1: Import needed Python modules, then set codegen
 #         and compile-time parameters.
@@ -16,12 +18,14 @@ import os
 import shutil
 
 import nrpy.helpers.parallel_codegen as pcg
+import nrpy.helpers.parallelization.cuda_utilities as cudautils
 import nrpy.infrastructures.BHaH.CurviBoundaryConditions.CurviBoundaryConditions as cbc
 import nrpy.infrastructures.BHaH.diagnostics.progress_indicator as progress
 import nrpy.params as par
 from nrpy.helpers.generic import copy_files
 from nrpy.infrastructures.BHaH import (
     BHaH_defines_h,
+    BHaH_device_defines_h,
     CodeParameters,
     Makefile_helpers,
     checkpointing,
@@ -36,7 +40,50 @@ from nrpy.infrastructures.BHaH import (
 from nrpy.infrastructures.BHaH.general_relativity import BSSN, TOVola
 from nrpy.infrastructures.BHaH.MoLtimestepping import MoL_register_all
 
+parser = argparse.ArgumentParser(
+    description="NRPyElliptic Solver for Conformally Flat BBH initial data"
+)
+parser.add_argument(
+    "--parallelization",
+    type=str,
+    help="Parallelization strategy to use (e.g. openmp, cuda).",
+    default="openmp",
+)
+parser.add_argument(
+    "--floating_point_precision",
+    type=str,
+    help="Floating point precision (e.g. float, double).",
+    default="double",
+)
+parser.add_argument(
+    "--disable_intrinsics",
+    action="store_true",
+    help="Flag to disable hardware intrinsics",
+    default=False,
+)
+parser.add_argument(
+    "--disable_rfm_precompute",
+    action="store_true",
+    help="Flag to disable RFM precomputation.",
+    default=False,
+)
+args = parser.parse_args()
+
+# Code-generation-time parameters:
+fp_type = args.floating_point_precision.lower()
+parallelization = args.parallelization.lower()
+enable_intrinsics = not args.disable_intrinsics
+enable_rfm_precompute = not args.disable_rfm_precompute
+
+if parallelization not in ["openmp", "cuda"]:
+    raise ValueError(
+        f"Invalid parallelization strategy: {parallelization}. "
+        "Choose 'openmp' or 'cuda'."
+    )
+
 par.set_parval_from_str("Infrastructure", "BHaH")
+par.set_parval_from_str("parallelization", parallelization)
+par.set_parval_from_str("fp_type", fp_type)
 
 # Code-generation-time parameters:
 project_name = "hydro_without_hydro"
@@ -44,7 +91,6 @@ IDtype = "TOVola_interp"
 CoordSystem = "Spherical"
 LapseEvolutionOption = "HarmonicSlicing"
 ShiftEvolutionOption = "Frozen"
-fp_type = "double"
 grid_physical_size = 7.5
 diagnostics_output_every = 0.25
 default_checkpoint_every = 2.0
@@ -54,11 +100,9 @@ Nxx_dict = {
     # "SinhSpherical": [72, 12, 2],
     # "Cartesian": [64, 64, 64],
 }
-enable_rfm_precompute = True
 MoL_method = "RK4"
 fd_order = 4
 radiation_BC_fd_order = 4
-enable_intrinsics = True
 separate_Ricci_and_BSSN_RHS = True
 parallel_codegen_enable = True
 enable_fd_functions = True
@@ -66,6 +110,11 @@ enable_KreissOliger_dissipation = False
 enable_CAKO = True
 enable_T4munu = True
 boundary_conditions_desc = "outgoing radiation"
+
+set_of_CoordSystems = {CoordSystem}
+NUMGRIDS = len(set_of_CoordSystems)
+num_cuda_streams = NUMGRIDS
+par.adjust_CodeParam_default("NUMGRIDS", NUMGRIDS)
 
 OMP_collapse = 1
 if "Spherical" in CoordSystem:
@@ -82,7 +131,6 @@ shutil.rmtree(project_dir, ignore_errors=True)
 
 par.set_parval_from_str("parallel_codegen_enable", parallel_codegen_enable)
 par.set_parval_from_str("fd_order", fd_order)
-par.set_parval_from_str("fp_type", fp_type)
 par.set_parval_from_str("CoordSystem_to_register_CodeParameters", CoordSystem)
 par.adjust_CodeParam_default("t_final", t_final)
 
@@ -90,6 +138,11 @@ par.adjust_CodeParam_default("t_final", t_final)
 #########################################################
 # STEP 2: Declare core C functions & register each to
 #         cfc.CFunction_dict["function_name"]
+if parallelization == "cuda":
+    cudautils.register_CFunctions_HostDevice__operations()
+    cudautils.register_CFunction_find_global_minimum()
+    cudautils.register_CFunction_find_global_sum()
+
 TOVola.TOVola_interp.register_CFunction_TOVola_interp()
 TOVola.TOVola_solve.register_CFunction_TOVola_solve()
 BSSN.initial_data.register_CFunction_initial_data(
@@ -117,7 +170,7 @@ TOVola_solve(commondata, &ID_persist);
 )
 
 numerical_grids_and_timestep.register_CFunctions(
-    set_of_CoordSystems={CoordSystem},
+    set_of_CoordSystems=set_of_CoordSystems,
     list_of_grid_physical_sizes=[grid_physical_size],
     Nxx_dict=Nxx_dict,
     enable_rfm_precompute=enable_rfm_precompute,
@@ -125,7 +178,7 @@ numerical_grids_and_timestep.register_CFunctions(
 )
 
 BSSN.diagnostics.register_CFunction_diagnostics(
-    set_of_CoordSystems={CoordSystem},
+    set_of_CoordSystems=set_of_CoordSystems,
     default_diagnostics_out_every=diagnostics_output_every,
     enable_psi4_diagnostics=False,
     grid_center_filename_tuple=("out0d-conv_factor%.2f.txt", "convergence_factor"),
@@ -141,7 +194,7 @@ BSSN.diagnostics.register_CFunction_diagnostics(
 )
 if enable_rfm_precompute:
     rfm_precompute.register_CFunctions_rfm_precompute(
-        set_of_CoordSystems={CoordSystem},
+        set_of_CoordSystems=set_of_CoordSystems,
     )
 BSSN.rhs_eval.register_CFunction_rhs_eval(
     CoordSystem=CoordSystem,
@@ -184,7 +237,7 @@ if __name__ == "__main__":
     pcg.do_parallel_codegen()
 
 cbc.CurviBoundaryConditions_register_C_functions(
-    set_of_CoordSystems={CoordSystem},
+    set_of_CoordSystems=set_of_CoordSystems,
     radiation_BC_fd_order=radiation_BC_fd_order,
 )
 rhs_string = ""
@@ -230,11 +283,27 @@ cmdline_input_and_parfiles.generate_default_parfile(
 cmdline_input_and_parfiles.register_CFunction_cmdline_input_and_parfile_parser(
     project_name=project_name, cmdline_inputs=["convergence_factor"]
 )
+gpu_defines_filename = BHaH_device_defines_h.output_device_headers(
+    project_dir, num_streams=num_cuda_streams
+)
 BHaH_defines_h.output_BHaH_defines_h(
     project_dir=project_dir,
     enable_intrinsics=enable_intrinsics,
+    intrinsics_header_lst=(
+        ["cuda_intrinsics.h"] if parallelization == "cuda" else ["simd_intrinsics.h"]
+    ),
     enable_rfm_precompute=enable_rfm_precompute,
     fin_NGHOSTS_add_one_for_upwinding_or_KO=True,
+    DOUBLE_means="double" if fp_type == "float" else "REAL",
+    restrict_pointer_type="*" if parallelization == "cuda" else "*restrict",
+    supplemental_defines_dict=(
+        {
+            "C++/CUDA safe restrict": "#define restrict __restrict__",
+            "GPU Header": f'#include "{gpu_defines_filename}"',
+        }
+        if parallelization == "cuda"
+        else {}
+    ),
 )
 
 main_c.register_CFunction_main_c(
@@ -250,18 +319,34 @@ griddata_commondata.register_CFunction_griddata_free(
 if enable_intrinsics:
     copy_files(
         package="nrpy.helpers",
-        filenames_list=["simd_intrinsics.h"],
+        filenames_list=(
+            ["cuda_intrinsics.h"]
+            if parallelization == "cuda"
+            else ["simd_intrinsics.h"]
+        ),
         project_dir=project_dir,
         subdirectory="intrinsics",
     )
-Makefile_helpers.output_CFunctions_function_prototypes_and_construct_Makefile(
-    project_dir=project_dir,
-    project_name=project_name,
-    exec_or_library_name=project_name,
-    compiler_opt_option="default",
-    addl_CFLAGS=["$(shell gsl-config --cflags)"],
-    addl_libraries=["$(shell gsl-config --libs)"],
-)
+if parallelization == "cuda":
+    Makefile_helpers.output_CFunctions_function_prototypes_and_construct_Makefile(
+        project_dir=project_dir,
+        project_name=project_name,
+        exec_or_library_name=project_name,
+        CC="nvcc",
+        src_code_file_ext="cu",
+        compiler_opt_option="nvcc",
+        addl_CFLAGS=["$(shell gsl-config --cflags)"],
+        addl_libraries=["$(shell gsl-config --libs)"],
+    )
+else:
+    Makefile_helpers.output_CFunctions_function_prototypes_and_construct_Makefile(
+        project_dir=project_dir,
+        project_name=project_name,
+        exec_or_library_name=project_name,
+        compiler_opt_option="default",
+        addl_CFLAGS=["$(shell gsl-config --cflags)"],
+        addl_libraries=["$(shell gsl-config --libs)"],
+    )
 print(
     f"Finished! Now go into project/{project_name} and type `make` to build, then ./{project_name} to run."
 )
