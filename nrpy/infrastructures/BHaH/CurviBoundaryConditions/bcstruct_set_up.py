32c2c2f0085d045727613206456d80eb483c5832
zachetienne <zachetie@gmail.com>
2025-06-21 08:37:09 -0700

    nrpy/infrastructures/BHaH/CurviBoundaryConditions/bcstruct_set_up.py: make mask-aware. Also clang-format and add comments.

--- nrpy/infrastructures/BHaH/CurviBoundaryConditions/bcstruct_set_up.py
+++ nrpy/infrastructures/BHaH/CurviBoundaryConditions/bcstruct_set_up.py
@@ -507,7 +507,7 @@ Step 2: Set up outer boundary structs bcstruct->outer_bc_array[which_gz][face][i
     )
 
     # Setup host-side struct to populate before copying to device
-    body = r"""
+    body = """
   ////////////////////////////////////////
   // STEP 1: SET UP INNER BOUNDARY STRUCTS
   // First count the number of inner boundary points and allocate memory for inner_bc_array.
@@ -515,7 +515,12 @@ Step 2: Set up outer boundary structs bcstruct->outer_bc_array[which_gz][face][i
     int num_inner = 0;
     LOOP_OMP("omp parallel for reduction(+:num_inner)", i0, 0, Nxx_plus_2NGHOSTS0, i1, 0, Nxx_plus_2NGHOSTS1, i2, 0, Nxx_plus_2NGHOSTS2) {
       const int i0i1i2[3] = {i0, i1, i2};
-      if (!IS_IN_GRID_INTERIOR(i0i1i2, Nxx_plus_2NGHOSTS0, Nxx_plus_2NGHOSTS1, Nxx_plus_2NGHOSTS2, NGHOSTS)) {
+"""
+    mask_check = ""
+    if enable_masks:
+        mask_check = "&& mask[IDX3(i0, i1, i2)] != UNSET"
+    body += f"if (!IS_IN_GRID_INTERIOR(i0i1i2, Nxx_plus_2NGHOSTS0, Nxx_plus_2NGHOSTS1, Nxx_plus_2NGHOSTS2, NGHOSTS) {mask_check}) {{\n"
+    body += """
         REAL x0x1x2_inbounds[3];
         int i0i1i2_inbounds[3];
         EigenCoord_set_x0x1x2_inbounds__i0i1i2_inbounds_single_pt(commondata, params, xx, i0, i1, i2, x0x1x2_inbounds, i0i1i2_inbounds);
@@ -540,7 +545,9 @@ Step 2: Set up outer boundary structs bcstruct->outer_bc_array[which_gz][face][i
     int which_inner = 0;
     LOOP_NOOMP(i0, 0, Nxx_plus_2NGHOSTS0, i1, 0, Nxx_plus_2NGHOSTS1, i2, 0, Nxx_plus_2NGHOSTS2) {
       const int i0i1i2[3] = {i0, i1, i2};
-      if (!IS_IN_GRID_INTERIOR(i0i1i2, Nxx_plus_2NGHOSTS0, Nxx_plus_2NGHOSTS1, Nxx_plus_2NGHOSTS2, NGHOSTS)) {
+"""
+    body += f"if (!IS_IN_GRID_INTERIOR(i0i1i2, Nxx_plus_2NGHOSTS0, Nxx_plus_2NGHOSTS1, Nxx_plus_2NGHOSTS2, NGHOSTS) {mask_check}) {{\n"
+    body += r"""
         REAL x0x1x2_inbounds[3];
         int i0i1i2_inbounds[3];
         EigenCoord_set_x0x1x2_inbounds__i0i1i2_inbounds_single_pt(commondata, params, xx, i0, i1, i2, x0x1x2_inbounds, i0i1i2_inbounds);
@@ -564,30 +571,39 @@ Step 2: Set up outer boundary structs bcstruct->outer_bc_array[which_gz][face][i
   // First set up loop bounds for outer boundary condition updates,
   //   store to bc_info->bc_loop_bounds[which_gz][face][]. Also
   //   allocate memory for outer_bc_array[which_gz][face][]:
-  int imin[3] = { NGHOSTS, NGHOSTS, NGHOSTS };
-  int imax[3] = { Nxx_plus_2NGHOSTS0-NGHOSTS, Nxx_plus_2NGHOSTS1-NGHOSTS, Nxx_plus_2NGHOSTS2-NGHOSTS };
-  for(int which_gz=0;which_gz<NGHOSTS;which_gz++) {
-    const int x0min_face_range[6] = { imin[0]-1,imin[0], imin[1],imax[1], imin[2],imax[2] };  imin[0]--;
-    const int x0max_face_range[6] = { imax[0],imax[0]+1, imin[1],imax[1], imin[2],imax[2] };  imax[0]++;
-    const int x1min_face_range[6] = { imin[0],imax[0], imin[1]-1,imin[1], imin[2],imax[2] };  imin[1]--;
-    const int x1max_face_range[6] = { imin[0],imax[0], imax[1],imax[1]+1, imin[2],imax[2] };  imax[1]++;
-    const int x2min_face_range[6] = { imin[0],imax[0], imin[1],imax[1], imin[2]-1,imin[2] };  imin[2]--;
-    const int x2max_face_range[6] = { imin[0],imax[0], imin[1],imax[1], imax[2],imax[2]+1 };  imax[2]++;
+  int imin[3] = {NGHOSTS, NGHOSTS, NGHOSTS};
+  int imax[3] = {Nxx_plus_2NGHOSTS0 - NGHOSTS, Nxx_plus_2NGHOSTS1 - NGHOSTS, Nxx_plus_2NGHOSTS2 - NGHOSTS};
+  for (int which_gz = 0; which_gz < NGHOSTS; which_gz++) {
+    const int x0min_face_range[6] = {imin[0] - 1, imin[0], imin[1], imax[1], imin[2], imax[2]};
+    imin[0]--;
+    const int x0max_face_range[6] = {imax[0], imax[0] + 1, imin[1], imax[1], imin[2], imax[2]};
+    imax[0]++;
+    const int x1min_face_range[6] = {imin[0], imax[0], imin[1] - 1, imin[1], imin[2], imax[2]};
+    imin[1]--;
+    const int x1max_face_range[6] = {imin[0], imax[0], imax[1], imax[1] + 1, imin[2], imax[2]};
+    imax[1]++;
+    const int x2min_face_range[6] = {imin[0], imax[0], imin[1], imax[1], imin[2] - 1, imin[2]};
+    imin[2]--;
+    const int x2max_face_range[6] = {imin[0], imax[0], imin[1], imax[1], imax[2], imax[2] + 1};
+    imax[2]++;
 
-    int face=0;
+    int face = 0;
     ////////////////////////
     // x0min and x0max faces: Allocate memory for outer_bc_array and set bc_loop_bounds:
     //                        Note that x0min and x0max faces have exactly the same size.
     //                   Also, note that face/2 --v   offsets this factor of 2 ------------------------------------------v
-    bcstruct->pure_outer_bc_array[3*which_gz + face/2] = (outerpt_bc_struct *)malloc(sizeof(outerpt_bc_struct) * 2 *
-                                                                                             ((x0min_face_range[1]-x0min_face_range[0]) *
-                                                                                              (x0min_face_range[3]-x0min_face_range[2]) *
-                                                                                              (x0min_face_range[5]-x0min_face_range[4])));
+    bcstruct->pure_outer_bc_array[3 * which_gz + face / 2] = (outerpt_bc_struct *)malloc(
+        sizeof(outerpt_bc_struct) * 2 *
+        ((x0min_face_range[1] - x0min_face_range[0]) * (x0min_face_range[3] - x0min_face_range[2]) * (x0min_face_range[5] - x0min_face_range[4])));
     // x0min face: Can't set bc_info->bc_loop_bounds[which_gz][face] = { i0min,i0max, ... } since it's not const :(
-    for(int i=0;i<6;i++) { bcstruct->bc_info.bc_loop_bounds[which_gz][face][i] = x0min_face_range[i]; }
+    for (int i = 0; i < 6; i++) {
+      bcstruct->bc_info.bc_loop_bounds[which_gz][face][i] = x0min_face_range[i];
+    } // END LOOP over all six faces of the grid
     face++;
     // x0max face: Set loop bounds & allocate memory for outer_bc_array:
-    for(int i=0;i<6;i++) { bcstruct->bc_info.bc_loop_bounds[which_gz][face][i] = x0max_face_range[i]; }
+    for (int i = 0; i < 6; i++) {
+      bcstruct->bc_info.bc_loop_bounds[which_gz][face][i] = x0max_face_range[i];
+    } // END LOOP over all six faces of the grid
     face++;
     ////////////////////////
 
@@ -595,35 +611,40 @@ Step 2: Set up outer boundary structs bcstruct->outer_bc_array[which_gz][face][i
     // x1min and x1max faces: Allocate memory for outer_bc_array and set bc_loop_bounds:
     //                        Note that x1min and x1max faces have exactly the same size.
     //                   Also, note that face/2 --v   offsets this factor of 2 ------------------------------------------v
-    bcstruct->pure_outer_bc_array[3*which_gz + face/2] = (outerpt_bc_struct *)malloc(sizeof(outerpt_bc_struct) * 2 *
-                                                                                             ((x1min_face_range[1]-x1min_face_range[0]) *
-                                                                                              (x1min_face_range[3]-x1min_face_range[2]) *
-                                                                                              (x1min_face_range[5]-x1min_face_range[4])));
+    bcstruct->pure_outer_bc_array[3 * which_gz + face / 2] = (outerpt_bc_struct *)malloc(
+        sizeof(outerpt_bc_struct) * 2 *
+        ((x1min_face_range[1] - x1min_face_range[0]) * (x1min_face_range[3] - x1min_face_range[2]) * (x1min_face_range[5] - x1min_face_range[4])));
     // x1min face: Can't set bc_info->bc_loop_bounds[which_gz][face] = { i0min,i0max, ... } since it's not const :(
-    for(int i=0;i<6;i++) { bcstruct->bc_info.bc_loop_bounds[which_gz][face][i] = x1min_face_range[i]; }
+    for (int i = 0; i < 6; i++) {
+      bcstruct->bc_info.bc_loop_bounds[which_gz][face][i] = x1min_face_range[i];
+    } // END LOOP over all six faces of the grid
     face++;
     // x1max face: Set loop bounds & allocate memory for outer_bc_array:
-    for(int i=0;i<6;i++) { bcstruct->bc_info.bc_loop_bounds[which_gz][face][i] = x1max_face_range[i]; }
+    for (int i = 0; i < 6; i++) {
+      bcstruct->bc_info.bc_loop_bounds[which_gz][face][i] = x1max_face_range[i];
+    } // END LOOP over all six faces of the grid
     face++;
     ////////////////////////
-
 
     ////////////////////////
     // x2min and x2max faces: Allocate memory for outer_bc_array and set bc_loop_bounds:
     //                        Note that x2min and x2max faces have exactly the same size.
     //                   Also, note that face/2 --v   offsets this factor of 2 ------------------------------------------v
-    bcstruct->pure_outer_bc_array[3*which_gz + face/2] = (outerpt_bc_struct *)malloc(sizeof(outerpt_bc_struct) * 2 *
-                                                                                             ((x2min_face_range[1]-x2min_face_range[0]) *
-                                                                                              (x2min_face_range[3]-x2min_face_range[2]) *
-                                                                                              (x2min_face_range[5]-x2min_face_range[4])));
+    bcstruct->pure_outer_bc_array[3 * which_gz + face / 2] = (outerpt_bc_struct *)malloc(
+        sizeof(outerpt_bc_struct) * 2 *
+        ((x2min_face_range[1] - x2min_face_range[0]) * (x2min_face_range[3] - x2min_face_range[2]) * (x2min_face_range[5] - x2min_face_range[4])));
     // x2min face: Can't set bc_info->bc_loop_bounds[which_gz][face] = { i0min,i0max, ... } since it's not const :(
-    for(int i=0;i<6;i++) { bcstruct->bc_info.bc_loop_bounds[which_gz][face][i] = x2min_face_range[i]; }
+    for (int i = 0; i < 6; i++) {
+      bcstruct->bc_info.bc_loop_bounds[which_gz][face][i] = x2min_face_range[i];
+    } // END LOOP over all six faces of the grid
     face++;
     // x2max face: Set loop bounds & allocate memory for outer_bc_array:
-    for(int i=0;i<6;i++) { bcstruct->bc_info.bc_loop_bounds[which_gz][face][i] = x2max_face_range[i]; }
+    for (int i = 0; i < 6; i++) {
+      bcstruct->bc_info.bc_loop_bounds[which_gz][face][i] = x2max_face_range[i];
+    } // END LOOP over all six faces of the grid
     face++;
     ////////////////////////
-  }
+  } // END LOOP over ghost zones, from inner to outer.
 
   for(int which_gz=0;which_gz<NGHOSTS;which_gz++) for(int dirn=0;dirn<3;dirn++) {
       int idx2d = 0;
