445ffd91dd06e7974041a8495286d896ff5f2aa4
zachetienne <zachetie@gmail.com>
2025-10-23 10:47:57 -0700

    nrpy/infrastructures/BHaH/general_relativity/constraints_eval.py: work on making RbarDD a pointwise computation for constraints_eval

--- nrpy/infrastructures/BHaH/general_relativity/constraints_eval.py
+++ nrpy/infrastructures/BHaH/general_relativity/constraints_eval.py
@@ -12,10 +12,30 @@ from typing import Union, cast
 import nrpy.c_codegen as ccg
 import nrpy.c_function as cfc
 import nrpy.finite_difference as fin
+import nrpy.grid as gri
 import nrpy.helpers.parallel_codegen as pcg
 import nrpy.params as par
 from nrpy.equations.general_relativity.BSSN_constraints import BSSN_constraints
 from nrpy.infrastructures import BHaH
+
+
+def register_CFunction_Ricci_onept():
+    if pcg.pcg_registration_phase():
+        print("registering!", f"{__name__}.{cast(FT, cfr()).f_code.co_name}", locals())
+        pcg.register_func_call(f"{__name__}.{cast(FT, cfr()).f_code.co_name}", locals())
+        return None
+    cfc.register_CFunction(
+        subdirectory="diagnostics",
+        enable_simd=True,
+        includes=["stdlib.h"],
+        prefunc="// blah",
+        desc="um hi",
+        cfunc_type="void",
+        name="Ricci_onept",
+        params="commondata_struct *restrict commondata",
+        include_CodeParameters_h=True,
+        body=r"""printf("hello world);\n""",
+    )
 
 
 def register_CFunction_constraints_eval(
@@ -35,6 +55,8 @@ def register_CFunction_constraints_eval(
     :return: None if in registration phase, else the updated NRPy environment.
     """
     if pcg.pcg_registration_phase():
+        # register_CFunction_Ricci_onept()
+        print("registering0!", f"{__name__}.{cast(FT, cfr()).f_code.co_name}", locals())
         pcg.register_func_call(f"{__name__}.{cast(FT, cfr()).f_code.co_name}", locals())
         return None
 
@@ -51,8 +73,21 @@ def register_CFunction_constraints_eval(
     name = "constraints_eval"
     params = "const commondata_struct *restrict commondata, const params_struct *restrict params, const rfm_struct *restrict rfmstruct, const REAL *restrict in_gfs, REAL *restrict diagnostic_gfs"
     Bcon = BSSN_constraints[
-        CoordSystem + "_rfm_precompute" + ("_T4munu" if enable_T4munu else "")
+        CoordSystem
+        + "_rfm_precompute_RbarDD_gridfunctions"
+        + ("_T4munu" if enable_T4munu else "")
     ]
+    # The above dictionary instantiation just populated gri.glb_gridfcs_dict
+    #  with RbarDD gridfunctions. As such, c_codegen will recognize them
+    #  as needing to be read from memory in the below codegen. We want to
+    #  1. make codegen of diagnostics fast (MUST enable RbarDD_gridfunctions)
+    #  2. avoid allocating additional diagnostics gridfunctions
+    #  So to these ends, this function will call another that sets RbarDD
+    #  *pointwise* (or more accurately, SIMD-wise).
+    if "RbarDD00" in gri.glb_gridfcs_dict:
+        for i in range(3):
+            for j in range(i, 3):
+                gri.glb_gridfcs_dict.pop(f"RbarDD{i}{j}")
     expr_list = [Bcon.H, Bcon.Msquared]
     body = BHaH.simple_loop.simple_loop(
         loop_body=ccg.c_codegen(
@@ -72,6 +107,12 @@ def register_CFunction_constraints_eval(
         enable_rfm_precompute=True,
         read_xxs=False,
         OMP_collapse=OMP_collapse,
+    )
+    _ = gri.register_gridfunctions_for_single_rank2(
+        "RbarDD",
+        symmetry="sym01",
+        group="AUXEVOL",
+        gf_array_name="auxevol_gfs",
     )
     prefunc = ""
     if enable_fd_functions:
