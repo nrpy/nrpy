98443d59aee56bb0c938da8e98cf1ee4a87d6bdf
Nishita Jadoo <njadoo@ncsu.edu>
2025-12-16 23:55:35 +0400

    Remove generated project directory from repository

--- project/superB_nrpyelliptic_conformally_flat/initial_guess_single_point.cpp
+++
@@ -1,9 +0,0 @@
-#include "BHaH_defines.h"
-
-/**
- * Compute initial guess at a single point.
- */
-void initial_guess_single_point(const REAL xx0, const REAL xx1, const REAL xx2, REAL *restrict uu_ID, REAL *restrict vv_ID) {
-  *uu_ID = 0;
-  *vv_ID = 0;
-} // END FUNCTION initial_guess_single_point
