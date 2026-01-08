5db786b92b3e00b78c13f38d4509a743f399a352
Nishita Jadoo <njadoo@ncsu.edu>
2026-01-08 12:08:53 +0400

    remove unncessary files

--- nrpy/infrastructures/superB/diagnostics/__init__.py
+++ nrpy/infrastructures/superB/diagnostics/__init__.py
@@ -3,7 +3,5 @@ from . import (
     diagnostics_nearest_1d_y_and_z_axes,
     diagnostics_nearest_2d_xy_and_yz_planes,
     diagnostics_nearest_grid_center,
-    progress_indicator,
-    sqrt_detgammahat_d3xx_volume_element,
 )
 from .diagnostics import _register_CFunction_diagnostics
